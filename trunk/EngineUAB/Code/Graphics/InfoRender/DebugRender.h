395b4d0a593aaf767b9f4c1ab7478378229c50b8
mcuscullola@gmail.com <mcuscullola@gmail.com@38b49c57-6b6f-6c24-9f89-8eb2c450b9d7>
2011-11-25 19:36:36 +0000

    Añadida la clase CDebugRender para dibujar la información de Debug.

---
+++ trunk/EngineUAB/Code/Graphics/InfoRender/DebugRender.h
@@ -0,0 +1,46 @@
+//----------------------------------------------------------------------------------
+// CDebugRender class
+// Author: Marc Cuscullola
+//
+// Description:
+// Clase encargada de pintar por pantalla la informacion de debug
+//----------------------------------------------------------------------------------
+#pragma once
+
+#ifndef _DEBUGRENDER_H
+#define _DEBUGRENDER_H
+
+#include "Math\Color.h"
+#include "Math\Vector2.h"
+#include "Utils\LerpAnimator1D.h"
+
+//---Forward Declarations---
+class CRenderManager;
+class CFontManager;
+class CTimer;
+//--------------------------
+
+class CDebugRender
+{
+public:
+	CDebugRender();
+	~CDebugRender();
+
+	void					Init						( CTimer *timer );
+	void					Update						( float deltaTime );
+	void					Render						( CRenderManager *rm, CFontManager *fm, CTimer *timer, CColor color = colWHITE );
+
+	void					SetVisible					( bool flag ) { m_bIsVisible = flag; }
+	bool					GetVisible					() const { return m_bIsVisible; }
+
+private:
+	void AnalizeSizeInfo ( CTimer *timer );
+
+	bool					m_bIsVisible;
+	Vect2i					m_SizeRectangle;
+
+	CLerpAnimator1D			m_LerpAnimator1D;
+};
+
+#endif
+
