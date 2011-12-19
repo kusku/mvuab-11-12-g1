2d6e6195cbbd90f6f829e9619ecac9c131a8173d
mcuscullola <mcuscullola@gmail.com>
2011-12-19 10:37:02 +0000

    Mejorado control de renderizado de las static meshes en CInstanceMesh. Modificado el script de exportación de mallas para soportar path relativos de las texturas.

--- EngineUAB/Code/Graphics/RenderableObjects/InstanceMesh.cpp
+++ EngineUAB/Code/Graphics/RenderableObjects/InstanceMesh.cpp
@@ -4,6 +4,7 @@
 #include "..\RenderManager.h"
 #include "Base.h"
 #include "Core.h"
+#include "Logger\Logger.h"
 #include "Math\Matrix44.h"
 
 #if defined(_DEBUG)
@@ -14,6 +15,13 @@ CInstanceMesh::CInstanceMesh(const std::string &Name, const std::string &CoreNam
 {
 	SetName(Name);
 	m_StaticMesh = CORE->GetStaticMeshManager()->GetResource(CoreName);
+
+#if defined(_DEBUG)
+	if( m_StaticMesh == NULL )
+	{
+		LOGGER->AddNewLog(ELL_WARNING, "No se ha encontrado la instancia a la static mesh: %", CoreName.c_str() );
+	}
+#endif
 }
 
 
@@ -24,17 +32,20 @@ CInstanceMesh::~CInstanceMesh()
 
 void CInstanceMesh::Render(CRenderManager *RM)
 {
-	Mat44f mat, rotYaw, rotPitch, rotRoll;
-	mat.SetIdentity();
-	rotYaw.SetIdentity();
-	rotPitch.SetIdentity();
-	rotRoll.SetIdentity();
-	mat.Translate( GetPosition() );
-	rotYaw.SetRotByAngleX( GetYaw() );
-	rotPitch.SetRotByAngleY( GetPitch() );
-	rotRoll.SetRotByAngleZ( GetRoll() );
-	mat = mat * rotYaw * rotPitch * rotRoll;
-	RM->SetTransform(mat);
+	if( m_StaticMesh != NULL )
+	{
+		Mat44f mat, rotYaw, rotPitch, rotRoll;
+		mat.SetIdentity();
+		rotYaw.SetIdentity();
+		rotPitch.SetIdentity();
+		rotRoll.SetIdentity();
+		mat.Translate( GetPosition() );
+		rotYaw.SetRotByAngleX( GetYaw() );
+		rotPitch.SetRotByAngleY( GetPitch() );
+		rotRoll.SetRotByAngleZ( GetRoll() );
+		mat = mat * rotYaw * rotPitch * rotRoll;
+		RM->SetTransform(mat);
 
-	m_StaticMesh->Render(RM);
+		m_StaticMesh->Render(RM);
+	}
 }
