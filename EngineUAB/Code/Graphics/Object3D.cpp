1d6f6d3a4354132d1bb41bd414db2fd94922585b
chaconc <chaconc@gmail.com>
2012-01-24 19:48:13 +0000

    Solucionando problema de pintar a pantalla el modelo animado

--- EngineUAB/Code/Graphics/Object3D.cpp
+++ EngineUAB/Code/Graphics/Object3D.cpp
@@ -13,16 +13,35 @@ CObject3D::CObject3D(const Vect3f& pos, const Vect3f& scale, float yaw, float pi
 	, m_Visible(true)	
 {
 	m_TechniqueName = "";
-	m_WorldMatrix.SetZero();
 }
 
 CObject3D::CObject3D()
 	: m_Position(Vect3f(0.f,0.f,0.f))
-	, m_Scale(Vect3f(0.f,0.f,0.f))
+	, m_Scale(Vect3f(1.f,1.f,1.f))
 	, m_fYaw(0.f)
 	, m_fPitch(0.f)
 	, m_fRoll(0.f)
 {
 	m_TechniqueName = "";
-	m_WorldMatrix.SetZero();
+}
+
+const Mat44f CObject3D::GetTransform() const
+{
+	Mat44f world = m44fIDENTITY;
+	Mat44f scale = m44fIDENTITY;
+	Mat44f rotationY = m44fIDENTITY;
+	Mat44f rotationX = m44fIDENTITY;
+	Mat44f rotationZ = m44fIDENTITY;
+	Mat44f translation = m44fIDENTITY;
+
+	translation.SetFromPos(m_Position);
+	scale.SetFromScale(m_Scale.x, m_Scale.y, m_Scale.z);
+	rotationX.SetFromAngleX(mathUtils::Deg2Rad<float>(m_fPitch));
+	rotationY.SetFromAngleY(mathUtils::Deg2Rad<float>(m_fYaw));
+	rotationZ.SetFromAngleZ(mathUtils::Deg2Rad<float>(m_fRoll));
+
+	//World = Scale * Rotation * Translation
+	world = scale * (rotationY * rotationX * rotationZ) * translation;
+
+	return world;
 }
--- EngineUAB/Code/Graphics/RenderableObjects/AnimatedModel/AnimatedInstanceModel.cpp
+++ EngineUAB/Code/Graphics/RenderableObjects/AnimatedModel/AnimatedInstanceModel.cpp
@@ -156,7 +156,8 @@ void CAnimatedInstanceModel::RenderModelByHardware(CRenderManager* RM, CEffectTe
 			m_TextureVector[0]->Activate(0);
 			//m_NormalTextureList[0]->Activate(1);
 	
-			m_AnimatedCoreModel->GetRenderableVertexs()->Render(RM, l_EffectTechnique, l_CalHardwareModel->getBaseVertexIndex(), 0, l_CalHardwareModel->getVertexCount(), l_CalHardwareModel->getStartIndex(),l_CalHardwareModel->getFaceCount());
+			m_AnimatedCoreModel->GetRenderableVertexs()->Render(RM, l_EffectTechnique, l_CalHardwareModel->getBaseVertexIndex(), 
+				0, l_CalHardwareModel->getVertexCount(), l_CalHardwareModel->getStartIndex(),l_CalHardwareModel->getFaceCount());
 		}
 	}
 }
