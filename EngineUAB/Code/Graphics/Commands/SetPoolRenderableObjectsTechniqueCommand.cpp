d612a4e1e81fd0bdefeadec7c856d91dec1a21c3
chaconc <chaconc@gmail.com>
2012-01-31 18:12:41 +0000

    Agregando Set Renderable Pool technique

---
+++ EngineUAB/Code/Graphics/Commands/SetPoolRenderableObjectsTechniqueCommand.cpp
@@ -0,0 +1,31 @@
+#include "SetPoolRenderableObjectsTechniqueCommand.h"
+#include "XML\XMLTreeNode.h"
+#include "RenderManager.h"
+#include "RenderableObjects\PoolRenderableObjectTechnique.h"
+#include "Core.h"
+#include "Base.h"
+#include "RenderableObjects\RenderableObjectTechniqueManager.h"
+#include "Utils\TemplatedVectorMapManager.h"
+
+#if defined(_DEBUG)
+#include "Memory\MemLeaks.h"
+#endif
+
+CSetPoolRenderableObjectsTechniqueCommand::CSetPoolRenderableObjectsTechniqueCommand()
+	: m_PoolRenderableObjectTechnique(NULL)
+{
+
+}
+
+CSetPoolRenderableObjectsTechniqueCommand::CSetPoolRenderableObjectsTechniqueCommand(CXMLTreeNode &Node)
+{
+	//<set_pool_renderable_objects_technique pool="generate_deferred_shading_pool_renderable_object_technique"/>
+	std::string poolName = Node.GetPszProperty("pool", "");
+
+	m_PoolRenderableObjectTechnique = CORE->GetROTManager()->GetPoolRenderableObjectTechniques().GetResource(poolName);
+}
+
+void CSetPoolRenderableObjectsTechniqueCommand::Execute(CRenderManager &RM)
+{
+	m_PoolRenderableObjectTechnique->Apply();
+}
---
+++ EngineUAB/Code/Graphics/Commands/SetPoolRenderableObjectsTechniqueCommand.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#ifndef _SET_POOL_RENDERABLE_OBJECTS_TECHNIQUE_COMMAND_H
+#define _SET_POOL_RENDERABLE_OBJECTS_TECHNIQUE_COMMAND_H
+
+#include "SceneRendererCommand.h"
+
+class CRenderManager;
+class CXMLTreeNode;
+class CPoolRenderableObjectTechnique;
+
+class CSetPoolRenderableObjectsTechniqueCommand : public CSceneRendererCommand
+{
+private:
+	CPoolRenderableObjectTechnique* m_PoolRenderableObjectTechnique;
+
+public:
+	CSetPoolRenderableObjectsTechniqueCommand();
+	CSetPoolRenderableObjectsTechniqueCommand(CXMLTreeNode &Node);
+	virtual ~CSetPoolRenderableObjectsTechniqueCommand();
+
+	virtual void	Execute		( CRenderManager &RM );
+
+
+};
+
+#endif
+
+
