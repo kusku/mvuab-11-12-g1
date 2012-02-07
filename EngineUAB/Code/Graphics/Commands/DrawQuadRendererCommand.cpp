a7a59871121c7909b971a69368d05075111eeae6
chaconc <chaconc@gmail.com>
2012-02-07 13:48:47 +0000

    Funcionando con Render Targets

--- EngineUAB/Code/Graphics/Commands/DrawQuadRendererCommand.cpp
+++ EngineUAB/Code/Graphics/Commands/DrawQuadRendererCommand.cpp
@@ -1,3 +1,7 @@
+
+#include <assert.h>
+#include <string>
+
 #include "DrawQuadRendererCommand.h"
 #include "XML\XMLTreeNode.h"
 #include "RenderManager.h"
@@ -6,7 +10,10 @@
 #include "Textures\TextureManager.h"
 #include "Base.h"
 #include "Core.h"
-#include <string>
+#include "RenderableObjects\RenderableObjectTechniqueManager.h"
+#include "RenderableObjects\RenderableObjectTechnique.h"
+#include "Vertexs\VertexType.h"
+#include "Math\Matrix44.h"
 
 #if defined(_DEBUG)
 #include "Memory\MemLeaks.h"
@@ -14,59 +21,62 @@
 
 CDrawQuadRendererCommand::CDrawQuadRendererCommand(CXMLTreeNode &Node)
 {
-	bool active = Node.GetBoolProperty("active", false);
-	SetActive(active);
-
 	uint16 l_Count = Node.GetNumChildren();
 	for(uint16 i=0; i<l_Count; ++i)
 	{
 		std::string l_Type = Node(i).GetName();
 		if( l_Type == "texture" )
 		{
-			int l_Stage = Node(i).GetIntProperty("stage_id", 0);
-			std::string l_Path = Node(i).GetPszProperty("path", "", false);
-			std::string l_File = Node(i).GetPszProperty("file", "", false);
+			int			l_Stage			= Node(i).GetIntProperty("stage_id", 0);
+			std::string l_File			= Node(i).GetPszProperty("file", "", false);
+			bool		l_LoadFromFile	= Node(i).GetBoolProperty("load_file", false, false);
+			CTexture*	l_Texture		= NULL;
 
-			if( l_File == "" && l_Path != "" )
+			assert(l_File != "");			
+
+			if( l_LoadFromFile )
 			{
-				CTexture *l_Texture = new CTexture();
-				bool nerr = l_Texture->Load(l_Path);
-				if( nerr )
+				l_Texture = CORE->GetTextureManager()->GetTexture(l_File);
+
+				if( l_Texture )
 				{
 					AddStageTexture(l_Stage, l_Texture);
-					l_Texture = NULL;
 				}
 				else
 				{
-					CHECKED_DELETE(l_Texture);
-					std::string errmsg = "CDrawQuadRendererCommand::CDrawQuadRendererCommand->No se ha podido crear la textura: " + l_Path;
-					LOGGER->AddNewLog(ELL_ERROR, errmsg.c_str());
-				}
-			}
-			else if( l_File != "" && l_Path == "" )
-			{
-				CTexture *l_Texture = CORE->GetTextureManager()->GetTexture(l_File);
-				if( l_Texture != NULL )
-				{
-					AddStageTexture(l_Stage, l_Texture);
-					l_Texture = NULL;
-				}
-				else
-				{
-					std::string errmsg = "CDrawQuadRendererCommand::CDrawQuadRendererCommand->No se ha podido encontrar la textura: " + l_File;
+					std::string errmsg = "CDrawQuadRendererCommand::CDrawQuadRendererCommand->No se ha podido crear la textura: " + l_File;
 					LOGGER->AddNewLog(ELL_ERROR, errmsg.c_str());
 				}
 			}
 			else
 			{
-				LOGGER->AddNewLog(ELL_ERROR, "CDrawQuadRendererCommand::CDrawQuadRendererCommand->No se encontrado ni path ni nombre de una textura.");
-			}
+				l_Texture = CORE->GetTextureManager()->GetResource(l_File);
 
+				if( l_Texture )
+				{
+					AddStageTexture(l_Stage, l_Texture);
+				}
+				else
+				{
+					std::string errmsg = "CDrawQuadRendererCommand::CDrawQuadRendererCommand->No se ha encontrado la textura: " + l_File;
+					LOGGER->AddNewLog(ELL_ERROR, errmsg.c_str());
+				}
+			}
+		}
+		else
+		{
+			std::string errmsg = "CDrawQuadRendererCommand::CDrawQuadRendererCommand->Comando no encontrado: " + l_Type;
+			LOGGER->AddNewLog(ELL_WARNING, errmsg.c_str());
 		}
 	}
 }
 
 void CDrawQuadRendererCommand::Execute(CRenderManager &RM)
 {
-	
+	this->ActivateTextures();
+
+	std::string technique = CORE->GetROTManager()->GetRenderableObjectTechniqueNameByVertexType(TCOLOREDTEXTURE1_VERTEX::GetVertexType());
+
+	RM.DrawColoredQuad2DTexturedInPixelsByEffectTechnique( CORE->GetROTManager()->GetResource(technique)->GetEffectTechnique(), colRED);
+															
 }
