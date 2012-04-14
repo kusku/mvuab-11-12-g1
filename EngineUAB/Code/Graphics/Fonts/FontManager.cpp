399ecb4b29c2ab1cfd20d60946dcdeba6dcfcb9a
mcuscullola <mcuscullola@gmail.com>
2012-04-14 18:34:28 +0000

    Añadido como ejemplo un botón en Test_GUI.

--- EngineUAB/Code/Graphics/Fonts/FontManager.cpp
+++ EngineUAB/Code/Graphics/Fonts/FontManager.cpp
@@ -256,7 +256,6 @@ uint32 CFontManager::DrawLiteral (uint32 x, uint32 y, const std::string & s_lit)
 	return DrawDefaultText(x, y, colGREEN, "Literal Error: %s", s_lit.c_str());
 }
 
-
 uint32 CFontManager::SizeX (const char * format, uint32 idFont)
 {
 	va_list args;
