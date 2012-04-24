031f47300203bffd13c3c1bd550e8924465fbd5c
mcuscullola@gmail.com <mcuscullola@gmail.com@38b49c57-6b6f-6c24-9f89-8eb2c450b9d7>
2012-04-24 13:31:22 +0000

    Añadida clase de creación de XML en GUIEditor.

---
+++ trunk/EngineUAB/Code/GUIEditor/InOut/XMLManager.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#ifndef _XML_MANAGER_H
+#define _XML_MANAGER_H
+
+#include <string>
+
+class CXMLTreeNode;
+class CGuiElement;
+class CGUIWindow;
+class CTexture;
+
+class CXMLManager
+{
+public:
+	static void SaveFiles(const std::string &filename );
+
+private:
+	static void SaveWindow(CXMLTreeNode &node, CGUIWindow *window, const std::string &path);
+	static void SavePointerMouse(CXMLTreeNode &node, CGuiElement *element);
+	static void SaveButton(CXMLTreeNode &node, CGuiElement *element, const std::string &path);
+
+	static std::string GetNameTexture(const std::string &file);
+	static void CopyTexture(CTexture *texture, const std::string &path);
+	static void CreateDirectories(const std::string &path);
+};
+
+#endif
