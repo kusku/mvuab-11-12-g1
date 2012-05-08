786d3572c24b6933aa836d7bcc3f00f288fc37a6
jordi.arenas.vigo <jordi.arenas.vigo@38b49c57-6b6f-6c24-9f89-8eb2c450b9d7>
2012-05-08 23:28:21 +0000

    Provisionalmente añadidos algunos Singlenton:
        -> SCRIPT : Esta quizás se mantenga por su gran uso en toda la aplicación
        -> ENTMGR : Maquina de estados
        -> DISPATCH: Mensajero entre mensajes entre estados de la maquina de estados

--- EngineUAB/Code/Base/Base.h
+++ EngineUAB/Code/Base/Base.h
@@ -9,9 +9,10 @@
 #define CHECKED_RELEASE(x) if(x){ x->Release(); x=NULL; }
 #define CHECKED_DELETE_ARRAY(x) if(x){ delete [] x; x=NULL; }
 
-#define CORE CCore::GetSingletonPtr()
-#define LOGGER CLogger::GetSingletonPtr()
-#define SCRIPT CScriptManager::GetSingletonPtr()
-
+#define CORE		CCore::GetSingletonPtr()
+#define LOGGER		CLogger::GetSingletonPtr()
+#define SCRIPT		CScriptManager::GetSingletonPtr()
+#define ENTMGR		CEntityManager::GetSingletonPtr()
+#define DISPACTH	CMessageDispatcher::GetSingletonPtr()
 
 #endif
