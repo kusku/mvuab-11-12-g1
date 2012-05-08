126426e571a21ac333a2c155871d4b7ba02ae073
jordi.arenas.vigo <jordi.arenas.vigo@38b49c57-6b6f-6c24-9f89-8eb2c450b9d7>
2012-05-08 23:11:14 +0000

    Actualización máquina de estados

--- EngineUAB/Code/Logic/StatesMachine/State.h
+++ EngineUAB/Code/Logic/StatesMachine/State.h
@@ -15,13 +15,13 @@ class CState
 {
 public:
 	// ------------- Constructors i Destructors ----------------
-					//CState		( void ) {};
-					~CState		( void ) {}
+					CState		( void ) {};
+	virtual			~CState		( void ) {};
 
 	// ------------- Mètodes Principals ---------------------
 	
 	// Se ejecuta en cada update del estado
-	virtual void	Execute		( T* )	= 0;
+	virtual void	Execute		( T* )						= 0;
 
 	// Se ejecuta cuando el estado es entrado
 	virtual void	OnEnter		( T* )						= 0;
@@ -29,11 +29,11 @@ public:
 	// Se ejecuta cuando el estado sale
 	virtual void	OnExit		( T* )						= 0;
 
-	// Pausa la ejecución del estado
-	virtual void	OnSuspend	( T* )						= 0;
+	//// Pausa la ejecución del estado
+	//virtual void	OnSuspend	( T* )						= 0;
 
-	// Permite la continuación de la ejecución del estado
-	virtual void	OnResume	( T* )						= 0;
+	//// Permite la continuación de la ejecución del estado
+	//virtual void	OnResume	( T* )						= 0;
 
 	// Este se ejecuta si el agente recibe un mensaje de un dispensador de mensajes
 	virtual bool	OnMessage	( T*, const Telegram& )		= 0;
