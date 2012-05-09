32d2cf61ad06c3c9aa83ec474a8c580dcc32d315
jordi.arenas.vigo <jordi.arenas.vigo@38b49c57-6b6f-6c24-9f89-8eb2c450b9d7>
2012-05-09 20:12:13 +0000

    Helper Classes que te permiten comprobar y recuperar datos de variables de Lua. Podria ser útil para modificar parámetros en Lua de forma rápida y estos deben ser cojidos en C++ para operar.

---
+++ EngineUAB/Code/Logic/LuaHelperFunctions.h
@@ -0,0 +1,156 @@
+#ifndef __LUA_HELPER_FUNCS__
+#define __LUA_HELPER_FUNCS__
+
+#include <string>
+
+//get rid of the annoying but unavoidable forcing int to bool warning
+#pragma warning (disable:4800)
+
+extern "C"
+{
+  #include <lua.h>
+  #include <lualib.h>
+  #include <lauxlib.h>
+}
+
+
+#include <string>
+//#include "misc/utils.h"
+//#include "misc/Stream_Utility_Functions.h"
+
+
+//----------------------------- PopLuaNumber-----------------------------------
+//
+//  Función templatizada para recuperar un numero de una pila de LUA
+//-----------------------------------------------------------------------------
+template <class T>
+inline T PopLuaNumber(lua_State* pL, const char* name)
+{
+	lua_settop(pL, 0);
+
+	lua_getglobal(pL, name);
+
+	// Chequeamos que la variable es del tipo correcto. Sino, se lanza una excepción
+	if (!lua_isnumber(pL, 1))
+	{
+		std::string err("<PopLuaNumber> Cannot retrieve: ");
+
+		throw std::runtime_error(err + name);
+	}
+
+	// Tomamos el valor, lo convertimos al tipo correcto y lo devolvemos
+	T val = (T)lua_tonumber(pL, 1);
+
+	// se elimina el valor de la pila
+	lua_pop(pL, 1);
+
+	return val;
+}
+
+//--------------------------- PopLuaString ------------------------------------
+//-----------------------------------------------------------------------------
+inline std::string PopLuaString(lua_State* pL, const char* name)
+{
+	lua_settop(pL, 0);
+
+	lua_getglobal(pL, name);
+
+	// Chequeamos que la variable es del tipo correcto. Sino, se lanza una excepción
+	if (!lua_isstring(pL, 1))
+	{
+		std::string err("<PopLuaString> Cannot retrieve: ");
+
+		throw std::runtime_error(err + name);
+	}
+
+	// Tomamos el valor, lo convertimos al tipo correcto y lo devolvemos
+	std::string s = lua_tostring(pL, 1);
+
+	// se elimina el valor de la pila
+	lua_pop(pL, 1);
+
+	return s;
+}
+
+//--------------------------- PopLuaBool ------------------------------------
+//-----------------------------------------------------------------------------
+inline bool PopLuaBool(lua_State* pL, const char* name)
+{
+	lua_settop(pL, 0);
+
+	lua_getglobal(pL, name);
+
+	// Chequeamos que la variable es del tipo correcto. Sino, se lanza una excepción
+	if (!lua_isstring(pL, 1))
+	{
+	std::string err("<PopLuaBool> Cannot retrieve: ");
+
+	throw std::runtime_error(err + name);
+	}
+
+	// Tomamos el valor, lo convertimos al tipo correcto y lo devolvemos
+	bool b = lua_toboolean(pL, 1);
+
+	// se elimina el valor de la pila
+	lua_pop(pL, 1);
+
+	return b;
+}
+
+//------------------------- LuaPopStringFieldFromTable ------------------------
+//-----------------------------------------------------------------------------
+inline std::string LuaPopStringFieldFromTable(lua_State* L, const char* key)
+{
+
+	// ponemos la key en la pila 
+	lua_pushstring(L, key);
+
+	// La tabla es ahora en -2 (key es en -1). lua_gettable quita la llave de la pila 
+	// y pone los datos encontrados en la localizació de la key en la pila 
+	lua_gettable(L, -2);
+
+	// Chequeamos que la variable es del tipo correcto. Sino, se lanza una excepción
+	if (!lua_isstring(L, -1))
+	{
+	std::string err("<LuaPopStringFieldFromTable> Cannot retrieve: ");
+
+	throw std::runtime_error(err + key);
+	}
+
+	// Tomamos los datos
+	std::string s = lua_tostring(L, -1);
+
+	lua_pop(L, 1);
+
+	return s;
+}
+
+//----------------------------- LuaPopNumberFieldFromTable --------------------
+//-----------------------------------------------------------------------------
+template <class T>
+inline T LuaPopNumberFieldFromTable(lua_State* L, const char* key)
+{
+	// ponemos la key en la pila 
+	lua_pushstring(L, key);
+
+	// La tabla es ahora en -2 (key es en -1). lua_gettable quita la llave de la pila 
+	// y pone los datos encontrados en la localizació de la key en la pila 
+	lua_gettable(L, -2);
+
+	// Chequeamos que la variable es del tipo correcto. Sino, se lanza una excepción
+	if (!lua_isnumber(L, -1))
+	{
+	std::string err("<LuaPopNumberFieldFromTable> Cannot retrieve: ");
+
+	throw std::runtime_error(err + key);
+	}
+
+	// Tomamos los datos
+	T val = (T)lua_tonumber(L, -1);
+
+	lua_pop(L, 1);
+
+	return val;
+}
+
+#endif __LUA_HELPER_FUNCS__
