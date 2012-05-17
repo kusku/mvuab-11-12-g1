8834c884d4f9da5baab9991121aeacc6e9cbe568
chaconc <chaconc@gmail.com>
2012-05-17 23:22:44 +0000

    Funcionando Enemy va por Waypoints (Agregando Funciones de Cross y Dot)

--- EngineUAB/Code/Base/Math/Vector3.h
+++ EngineUAB/Code/Base/Math/Vector3.h
@@ -133,7 +133,11 @@ public:
   inline T           GetAngleY   () const;
   inline T           GetAngleZ   () const;
   inline void        GetAngles   (T& angX, T& angY, T& angZ) const;
-         
+  
+  inline Vector3<T>	 GetXZFromAngle (float radians);
+
+  inline float		Dot		(const Vector3<T>& otro) const;
+  inline Vector3<T>	Cross	(const Vector3<T>& otro) const;
 
   // Interpolación lineal
   inline Vector3<T>& Lerp    (const Vector3<T>& otro, const T t);
