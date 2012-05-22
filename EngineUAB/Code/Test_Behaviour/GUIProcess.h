aa887cd12e8295fb2b3fb2fd9f125eabee35f008
jordi.arenas.vigo <jordi.arenas.vigo@38b49c57-6b6f-6c24-9f89-8eb2c450b9d7>
2012-05-22 22:10:13 +0000

    Nuevo proyecto de test de comportamiento en Lua

---
+++ EngineUAB/Code/Test_Behaviour/GUIProcess.h
@@ -0,0 +1,45 @@
+//----------------------------------------------------------------------------------
+// CGUIProcess class
+// Author: Marc Cuscullola
+//
+// Description:
+// Proceso que se encarga de gestionar la GUI del juego
+//----------------------------------------------------------------------------------
+#pragma once
+
+#ifndef _GUI_PROCESS_H_
+#define _GUI_PROCESS_H_
+
+#include "EngineProcess.h"
+#include "Cameras\StaticCamera.h"
+#include <Windows.h>
+
+class CRenderManager;
+class CThPSCamera;
+
+class CGUIProcess : public CEngineProcess
+{
+public:
+	CGUIProcess( HWND hWnd );
+	~CGUIProcess();
+	
+	bool	Init ();
+	void	Update ( float elapsedTime );
+	void	Render ( CRenderManager &RM );
+
+	//--Métodos para exponer en LUA-------
+	void	ChangeProcess		();
+	void	ExitGame			();
+
+private:
+	void	RegisterMethods ();
+	void	Release			();
+
+private:
+	CStaticCamera		m_StaticCamera;
+	CThPSCamera			*m_pThPSCamera;
+
+	HWND	m_hWnd;
+};
+
+#endif
---
+++ EngineUAB/Code/Test_Behaviour/TestBehaviour.cpp
@@ -0,0 +1,177 @@
+#include <Windows.h>
+#include "GUIProcess.h"
+#include "GameProcess.h"
+#include "Xml/XMLTreeNode.h"
+#include "Exceptions\Exception.h"
+#include "Engine.h"
+#include "Logger\Logger.h"
+#include "Core.h"
+#include "Base.h"
+#include "Scripting\ScriptManager.h"
+#include "TestBehaviourDefs.h"
+
+#if defined (_DEBUG)
+#include "Memory\MemLeaks.h"
+#endif
+
+#define APPLICATION_NAME	"Test Behaviour"
+
+CEngine			*g_pEngine = NULL;
+CGUIProcess		*g_pGUIProcess = NULL;
+CGameProcess	*g_pGameProcess = NULL;
+
+HWND			g_hWnd = NULL;
+
+void ShowErrorMessage (const std::string& message);
+
+//-----------------------------------------------------------------------------
+// Name: MsgProc()
+// Desc: The window's message handler
+//-----------------------------------------------------------------------------
+LRESULT WINAPI MsgProc( HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam )
+{
+	switch( msg )
+	{
+	case WM_DESTROY:
+		{
+			PostQuitMessage( 0 );
+			return 0;
+			break;
+		}			
+	case WM_KEYDOWN:
+		{
+			switch( wParam )
+			{
+			case VK_ESCAPE:
+				//Cleanup();
+				PostQuitMessage( 0 );
+				return 0;
+				break;
+			}
+			break;
+		}
+	case WM_GAME_PROCESS:
+		{
+			if( g_pGameProcess == NULL )
+			{
+				g_pGameProcess = new CGameProcess(g_hWnd);
+				g_pGameProcess->Init();
+			}
+
+			g_pEngine->SetProcess( g_pGameProcess );
+			CORE->SetProcess(g_pGameProcess);
+			CORE->SetGameMode(true);
+			break;
+		}
+	case WM_GUI_PROCESS:
+		{
+			if( g_pGUIProcess == NULL )
+			{
+				g_pGUIProcess = new CGUIProcess(g_hWnd);
+				g_pGUIProcess->Init();
+			}
+
+			g_pEngine->SetProcess( g_pGUIProcess );
+			CORE->SetProcess(g_pGUIProcess);
+			CORE->SetGameMode(false);
+			break;
+		}
+	}//end switch( msg )
+
+	return DefWindowProc( hWnd, msg, wParam, lParam );
+}
+
+//-----------------------------------------------------------------------
+// WinMain
+//-----------------------------------------------------------------------
+int APIENTRY WinMain(HINSTANCE _hInstance, HINSTANCE _hPrevInstance, LPSTR _lpCmdLine, int _nCmdShow)
+{
+	// Register the window class
+	WNDCLASSEX wc = {	sizeof(WNDCLASSEX), CS_CLASSDC, MsgProc, 0L, 0L, GetModuleHandle(NULL), NULL, NULL, NULL, NULL, APPLICATION_NAME, NULL };
+	
+	RegisterClassEx( &wc );
+
+	try
+	{
+		g_pEngine = new CEngine();
+		g_pEngine->LoadConfigXML("./Data/XML/engine.xml");
+
+		Vect2i position = g_pEngine->GetPosition();
+		Vect2i resolution = g_pEngine->GetResolution();
+
+		 // Create the application's window
+		g_hWnd = CreateWindow(	APPLICATION_NAME, APPLICATION_NAME, WS_OVERLAPPEDWINDOW, 
+			position.x, position.y, resolution.x, resolution.y, NULL, NULL, wc.hInstance, NULL );
+
+		if( INIT_GUI )
+		{
+			g_pGUIProcess = new CGUIProcess(g_hWnd);
+			g_pEngine->SetProcess(g_pGUIProcess);
+			g_pEngine->Init(g_hWnd);
+		
+			CORE->SetProcess(g_pGUIProcess);
+			CORE->SetGameMode(false);
+		}
+		else
+		{
+			g_pGameProcess = new CGameProcess(g_hWnd);
+			g_pEngine->SetProcess(g_pGameProcess);
+			g_pEngine->Init(g_hWnd);
+		
+			CORE->SetProcess(g_pGameProcess);
+			CORE->SetGameMode(true);
+		}
+		
+		ShowWindow( g_hWnd, SW_SHOWDEFAULT );
+		UpdateWindow( g_hWnd );
+		MSG msg;
+		ZeroMemory( &msg, sizeof(msg) );
+
+		//Bucle principal
+		while( msg.message != WM_QUIT )
+		{
+			if( PeekMessage( &msg, NULL, 0U, 0U, PM_REMOVE ) )
+			{
+				TranslateMessage( &msg );
+				DispatchMessage( &msg );
+			}
+			else
+			{
+				g_pEngine->Update();
+				g_pEngine->Render();
+			}
+		}
+	}
+	catch(CException &e)
+	{
+		ShowErrorMessage(e.GetDescription());
+	}
+
+	UnregisterClass( APPLICATION_NAME, wc.hInstance );
+
+	//Elimina los datos
+	CHECKED_DELETE(g_pEngine);
+	CHECKED_DELETE(g_pGUIProcess);
+	//CHECKED_DELETE(g_GameProcess);
+
+	return 0;
+}
+
+void ShowErrorMessage (const std::string& message)
+{
+	bool logSaved = false;
+
+    logSaved = LOGGER->SaveLogsInFile();
+
+    std::string end_message = "";
+    if (logSaved)   
+	{
+		end_message += "Sorry, Application failed. Logs saved\n";
+	}
+    else
+	{ 
+		end_message += "Sorry, Application failed. Logs could not be saved\n";
+	}
+    end_message += message;
+    MessageBox(0, end_message.c_str(), "FlostiProject Report", MB_OK | MB_ICONERROR);
+}
