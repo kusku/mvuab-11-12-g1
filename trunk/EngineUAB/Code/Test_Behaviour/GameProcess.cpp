329fdf9f708199a95eababa9d67d08572b10b755
jordi.arenas.vigo <jordi.arenas.vigo@38b49c57-6b6f-6c24-9f89-8eb2c450b9d7>
2012-05-24 15:04:46 +0000

    Ya funciona el proyecto como deberia. Ahora a por LUA!!!

--- trunk/EngineUAB/Code/Test_Behaviour/GameProcess.cpp
+++ trunk/EngineUAB/Code/Test_Behaviour/GameProcess.cpp
@@ -1,22 +1,28 @@
 #include "GameProcess.h"
+#include "TestBehaviourDefs.h"
+#include "Characters\PlayerDef.h"
+
 #include "Cameras\ThPSCamera.h"
 #include "Cameras\Camera.h"
+
 #include "RenderManager.h"
+#include "RenderableObjects\RenderableObjectsManager.h"
+#include "RenderableObjects\RenderableObjectsLayersManager.h"
 #include "Scripting\ScriptManager.h"
 
 #include "Characters\CharacterManager.h"
 #include "Characters\Character.h"
-#include "StatesMachine\ScriptedStateMachine.h"
-#include "DebugSceneBehaviour\Scene.h"
-
 #include "Characters\Player\Player.h"
 #include "Characters\Properties\Properties.h"
+#include "Characters\Properties\PropertiesManager.h"
 
-
+#include "StatesMachine\ScriptedStateMachine.h"
+#include "DebugSceneBehaviour\Scene.h"
+#include "SoundManager.h"
+#include "ActionToInput.h"
 
 #include "Core.h"
 #include "Base.h"
-#include "TestBehaviourDefs.h"
 
 #if defined (_DEBUG)
 #include "Memory/MemLeaks.h"
@@ -26,6 +32,7 @@ CGameProcess::CGameProcess(HWND hWnd)
 	: m_hWnd	( hWnd )
 	//, m_pPlayer ( NULL )
 {
+	m_pCharactersManager	= new CCharactersManager();
 	m_pScene = new CScene();
 }
 
@@ -56,11 +63,11 @@ bool CGameProcess::Init()
 	m_StaticCamera.SetYaw(0.0f);
 	m_StaticCamera.SetRoll(0.0f);
 
-	float l_Aspect = CORE->GetRenderManager()->GetAspectRatio();
-	m_pThPSCamera = new CThPSCamera(1.0f, 10000.f, 45.f * D3DX_PI / 180.f, l_Aspect, &m_StaticCamera, 10.0f, 0.f, "Static");
-	//m_pThPSCamera = new CThPSCamera( 1.0f, 10000.f, 45.f * D3DX_PI / 180.f, l_Aspect, m_Player, 12.0f, 4.f, "Wolf");
-	m_pCamera = static_cast<CCamera*>(m_pThPSCamera);
-	CORE->SetCamera(m_pCamera);
+	//float l_Aspect = CORE->GetRenderManager()->GetAspectRatio();
+	//m_pThPSCamera = new CThPSCamera(1.0f, 10000.f, 45.f * D3DX_PI / 180.f, l_Aspect, &m_StaticCamera, 10.0f, 0.f, "Static");
+	////m_pThPSCamera = new CThPSCamera( 1.0f, 10000.f, 45.f * D3DX_PI / 180.f, l_Aspect, m_Player, 12.0f, 4.f, "Wolf");
+	//m_pCamera = static_cast<CCamera*>(m_pThPSCamera);
+	//CORE->SetCamera(m_pCamera);
 
 	/*m_FreeCamera.SetPosition(Vect3f( 0.f, 10.f, 0.f));
 	m_FreeCamera.SetPitch(-D3DX_PI/6);
@@ -75,14 +82,35 @@ bool CGameProcess::Init()
 	
 	// Creamos la escena de debug. Esto solo si no se tiene escenario
 	m_pScene->Init();
-
-	//m_PM = new CPropertiesManager();
-
+	
 	// Inicializa el gestor de player y enemigos. Carga propiedades y estados de todo.
-	/*if ( !m_pCharactersManager->Initialize ( ) )
+	if ( !m_pCharactersManager->Initialize ( ) )
 	{
 		return false;
-	}*/
+	}
+
+	m_Player = m_pCharactersManager->GetPlayer();
+	m_Player->SetPosition(Vect3f( 0.f, 10.f, 0.f));
+	m_Player->SetPitch(-D3DX_PI/8);
+	m_Player->SetYaw(0.0f);
+	m_Player->SetRoll(0.0f);
+	m_Player->Init();
+
+	float l_Aspect = CORE->GetRenderManager()->GetAspectRatio();
+	m_pThPSCamera = new CThPSCamera( 1.0f, 10000.f, 45.f * D3DX_PI / 180.f, l_Aspect, m_Player, 12.0f, 4.f, "Wolf");
+	m_pCamera = static_cast<CCamera*>(m_pThPSCamera);
+	// Asignamos la camera activa al core
+	CORE->SetCamera( m_pCamera );
+
+	m_FreeCamera.SetPosition(Vect3f( 0.f, 10.f, 0.f));
+	m_FreeCamera.SetPitch(-D3DX_PI/6);
+	m_FreeCamera.SetYaw(0.0f);
+	m_FreeCamera.SetRoll(0.0f);
+
+	m_pThPSFreeCamera = new CThPSCamera( 1.0f, 10000.f, 45.f * D3DX_PI / 180.f, l_Aspect, &m_FreeCamera, 10.0f, 0.f, "Free");
+	m_pFreeCamera = static_cast<CCamera*>(m_pThPSFreeCamera);
+
+	m_Player->SetLockCamera( false );
 
 	/*CProperties* l_pPlayerProperties = new CProperties();
 	l_pPlayerProperties->SetAnimationInstance( "Caperucita1" );
@@ -102,14 +130,68 @@ bool CGameProcess::Init()
 	return true;
 }
 
-void CGameProcess::Update(float elapsedTime)
+void CGameProcess::Update( float _ElapsedTime )
 {
+	CORE->SetCamera( m_pCamera );
+	
+	m_pCharactersManager->Update ( _ElapsedTime );
+		
+	//m_Player->Update(_ElapsedTime, m_pCamera );
+	if( m_Player->GetLockCamera() )
+	{
+		m_FreeCamera.Update(_ElapsedTime ,m_pCamera);
+	}
+
+	CORE->GetRenderableObjectsLayersManager()->Update( _ElapsedTime );
+
+	UpdateInputs( _ElapsedTime );	
 }
 
 void CGameProcess::Render(CRenderManager &RM)
 {
 }
 
+void CGameProcess::UpdateInputs ( float _ElapsedTime )
+{
+	if( CORE->GetActionToInput()->DoAction("CommutacioCamera") )
+	{
+		if( m_pCamera == m_pThPSCamera )
+		{
+			m_pCamera = m_pThPSFreeCamera;
+			CORE->SetCamera( m_pThPSFreeCamera );
+			m_Player->SetLockCamera( true );
+		}
+		else
+		{
+			m_pCamera = m_pThPSCamera;
+			CORE->SetCamera( m_pThPSCamera );
+			m_Player->SetLockCamera( false );
+		}
+	}
+
+	/*if( m_pCamera == static_cast<CCamera*>(m_pThPSCamera) )
+	{
+		if( CORE->GetActionToInput()->DoAction(ACTION_MOVE_PLAYER_FOWARD) || 
+			CORE->GetActionToInput()->DoAction(ACTION_MOVE_PLAYER_BACK) ||
+			CORE->GetActionToInput()->DoAction(ACTION_MOVE_PLAYER_LEFT) ||
+			CORE->GetActionToInput()->DoAction(ACTION_MOVE_PLAYER_RIGHT))
+		{
+			if( m_AudioIsFade )
+			{
+				CORE->GetSoundManager()->FadeInSource(m_uAudioID, 0.5f);
+				m_AudioIsFade = false;
+			}
+		}
+		else
+		{
+			if( !m_AudioIsFade )
+			{
+				CORE->GetSoundManager()->FadeOutSource(m_uAudioID, 0.5f);
+				m_AudioIsFade = true;
+			}
+		}
+	}*/
+}
 //-------------------------------------
 //--Registrador de métodos en LUA------
 //-------------------------------------
