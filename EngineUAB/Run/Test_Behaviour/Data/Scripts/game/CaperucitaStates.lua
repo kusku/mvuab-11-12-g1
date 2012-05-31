a5a35315fad2c9b47ad52a2872d61a0fef895d2c
jordi.arenas.vigo <jordi.arenas.vigo@38b49c57-6b6f-6c24-9f89-8eb2c450b9d7>
2012-05-31 11:04:00 +0000

    xml's i scripts de lua para estados

---
+++ EngineUAB/Run/Test_Behaviour/Data/Scripts/game/CaperucitaStates.lua
@@ -0,0 +1,71 @@
+class 'CCaperucitaIdleState' (CState)
+	function CCaperucitaIdleState:__init() 
+		super()
+		print_logger(0, "Inicio del estado idle de la caperucita")
+	end
+
+	function CCaperucitaIdleState:Enter(_State)
+	
+	end
+	
+	function CCaperucitaIdleState:Execute(_State)
+	
+	end
+	
+	function CCaperucitaIdleState:Exit(_State)
+	
+	end
+	
+	-- function CCaperucitaIdleState:OnMessage(_State)
+	
+	-- end
+	
+	function CCaperucitaIdleState:__Finalize()
+	
+	end
+	
+	
+-- class 'CCaperucitaWalkState' (CState)
+	-- function CState:__init()
+		-- print_logger(0, "Inicio del estado walk del lobo")
+	
+	-- end
+	
+	-- function CCaperucitaWalkState:Enter()
+	
+	-- end
+	
+	-- function CCaperucitaWalkState:Execute()
+	
+	-- end
+	
+	-- function CCaperucitaWalkState:Exit()
+	
+	-- end
+	
+	-- function CCaperucitaWalkState:__Finalize()
+	
+	-- end
+	
+		
+-- class 'CCaperucitaRunState' (CState)
+	-- function CState:__init()
+		-- print_logger(0, "Inicio del estado run del lobo")
+	
+	-- end
+	
+	-- function CCaperucitaRunState:Enter()
+	
+	-- end
+	
+	-- function CCaperucitaRunState:Execute()
+	
+	-- end
+	
+	-- function CCaperucitaRunState:Exit()
+	
+	-- end
+	
+	-- function CCaperucitaRunState:__Finalize()
+	
+	-- end
---
+++ EngineUAB/Run/Test_Behaviour/Data/Scripts/game/WolfStates.lua
@@ -0,0 +1,67 @@
+class 'CWorlfIdleState' (CState)
+	function CState:__init()
+		print_logger(0, "Inicio del estado idle del lobo")
+	
+	end
+	
+	function CWorlfIdleState:Enter()
+	
+	end
+	
+	function CWorlfIdleState:Execute()
+	
+	end
+	
+	function CWorlfIdleState:Exit()
+	
+	end
+	
+	function CWorlfIdleState:__Finalize()
+	
+	end
+	
+	
+class 'CWorlfWalkState' (CState)
+	function CState:__init()
+		print_logger(0, "Inicio del estado walk del lobo")
+	
+	end
+	
+	function CWorlfIdleState:Enter()
+	
+	end
+	
+	function CWorlfIdleState:Execute()
+	
+	end
+	
+	function CWorlfIdleState:Exit()
+	
+	end
+	
+	function CWorlfIdleState:__Finalize()
+	
+	end
+	
+		
+class 'CWorlfRunState' (CState)
+	function CState:__init()
+		print_logger(0, "Inicio del estado run del lobo")
+	
+	end
+	
+	function CWorlfIdleState:Enter()
+	
+	end
+	
+	function CWorlfIdleState:Execute()
+	
+	end
+	
+	function CWorlfIdleState:Exit()
+	
+	end
+	
+	function CWorlfIdleState:__Finalize()
+	
+	end
