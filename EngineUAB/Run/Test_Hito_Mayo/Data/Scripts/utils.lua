6a632c62d8af33f8fb0c431d678f0915947fda5f
jordi_arenas <jordi_arenas@hotmail.com>
2012-06-07 18:17:32 +0000

    no pilla el waypoint manager del lua. lo comento

--- EngineUAB/Run/Test_Hito_Mayo/Data/Scripts/utils.lua
+++ EngineUAB/Run/Test_Hito_Mayo/Data/Scripts/utils.lua
@@ -1,4 +1,4 @@
 core = CSingleton_CCore.get_singleton()
 _guiM = core:get_gui_manager()
 _soundM = core:get_sound_manager()
-_waypointsM = core:get_waypoints_manager()
+--_waypointsM = core:get_waypoints_manager()
