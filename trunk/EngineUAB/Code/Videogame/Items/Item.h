dc27c20c3aba309e3226778d9f25911ff4a83f4b
mcuscullola@gmail.com <mcuscullola@gmail.com@38b49c57-6b6f-6c24-9f89-8eb2c450b9d7>
2012-06-07 17:40:32 +0000

    Añadidas clases de ítems en Videogame.

---
+++ trunk/EngineUAB/Code/Videogame/Items/Item.h
@@ -0,0 +1,20 @@
+//----------------------------------------------------------------------------------
+// CItem class
+// Author: Marc Cuscullola
+//
+// Description:
+// Clase base de un ítem
+//----------------------------------------------------------------------------------
+#pragma once
+
+#ifndef _ITEM_H_
+#define _ITEM_H_
+
+class CItem
+{
+public:
+	CItem();
+	~CItem();
+};
+
+#endif
