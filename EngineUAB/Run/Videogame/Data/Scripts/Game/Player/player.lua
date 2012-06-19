79cc1ee29de59f42d7ad167687091d06419b0bf5
mcuscullola <mcuscullola@gmail.com>
2012-06-19 16:05:55 +0000

    Modificado el ángulo de ataque.

--- EngineUAB/Run/Videogame/Data/Scripts/Game/Player/player.lua
+++ EngineUAB/Run/Videogame/Data/Scripts/Game/Player/player.lua
@@ -97,7 +97,7 @@ class 'CPlayer' (CCharacter)
 	
 	function CPlayer:detect_enemy()
 		local character_manager = get_game_process():get_character_manager()
-		local enemy = character_manager:search_target_enemy(20.0, math.pi / 4)
+		local enemy = character_manager:search_target_enemy(20.0, math.pi / 3)
 		character_manager.preview_target_enemy = enemy
 		return enemy
 	end
