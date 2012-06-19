1d8e39aff6d577c9c5f688e7f6d811dc68ed1564
jordi.arenas.vigo <jordi.arenas.vigo@38b49c57-6b6f-6c24-9f89-8eb2c450b9d7>
2012-06-19 01:23:56 +0000

    actualizados algunos estados después de comprobar que funcionan

--- trunk/EngineUAB/Run/Videogame/Data/Scripts/Game/Enemies/wolf_states/wolf_hit_state.lua
+++ trunk/EngineUAB/Run/Videogame/Data/Scripts/Game/Enemies/wolf_states/wolf_hit_state.lua
@@ -5,42 +5,36 @@ class 'CWolfHitState' (CState)
 	end
 
 	function CWolfHitState:OnEnter(_CCharacter)
-		print_logger(0, "CWolfHitState:Enter")
+		--print_logger(0, "CWolfHitState:Enter")
 		if not ( _CCharacter == nil ) then
 			local num = _CCharacter:get_animation_id("hit")
-			_CCharacter:get_animation_model():blend_cycle( num, 0.01 )
+			_CCharacter:get_animation_model():blend_cycle( num, 1 )
 		end
 		self.hit_animation_time = 0.0
 	end
 	
 	function CWolfHitState:Execute(_CCharacter)
-		-- print_logger(0, "CWolfHitState:Execute")
+		--print_logger(0, "CWolfHitState:Execute")
 		if ( self.hit_animation_time >= _CCharacter.animated_model:get_current_animation_duration("hit") ) then
 			print_logger(0, "CWolfHitState:Execute->Fi del hit")
+			_CCharacter:rest_life( _CCharacter.player.properties.strong )
 			_CCharacter.logic_fsm:revert_to_previous_state()
 		end
 		self.hit_animation_time = self.hit_animation_time + _CCharacter.elapsed_time
 	end
 	
 	function CWolfHitState:OnExit(_CCharacter)
-		print_logger(0, "CWolfHitState:Exit")
+		--print_logger(0, "CWolfHitState:Exit")
 		_CCharacter.hit_animation_time = 0.0
 		if not ( _CCharacter == nil ) then
 			num = _CCharacter:get_animation_id("hit")
 			_CCharacter:get_animation_model():clear_cycle( num, 0.7 )
 		end
+		print_logger(0, "CWolfHitState:OnExit->WOLF life : ".._CCharacter.properties.life)
 	end
 	
 	function CWolfHitState:OnMessage(_CCharacter, _Msg)
-		print_logger(0, "CWolfHitState:OnMessage")
-		if ( _Msg.msg == msg_attack ) then
-			print_logger(0, "Missatge acceptat per el llob")
-			-- If depend tipus d'atac... treu més o menys vida... --
-			-- - -- Ja l'he fet en l'attack _CCharacter:rest_life( 1 )
-			print_logger(0, "WOLF life : ".._CCharacter.properties.life)
-
-			return true
-		end
+		--print_logger(0, "CWolfHitState:OnMessage")
 		return false
 	end
 	
