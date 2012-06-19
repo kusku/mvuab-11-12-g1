1f6dcdd128d7548b6c27451a55778d68ed2758bb
jordi.arenas.vigo <jordi.arenas.vigo@38b49c57-6b6f-6c24-9f89-8eb2c450b9d7>
2012-06-19 12:39:51 +0000

    subido a 95% el % de fuerza para cambiar de estado HIT

--- trunk/EngineUAB/Run/Videogame/Data/Scripts/Game/Player/player_attack3_state.lua
+++ trunk/EngineUAB/Run/Videogame/Data/Scripts/Game/Player/player_attack3_state.lua
@@ -58,7 +58,7 @@ class 'CPlayerAttack3State' (CState)
 		if ( _Msg.msg == msg_attack ) then
 			local l_Enemy = get_game_process():get_character_manager():get_enemy_by_id(_Msg.sender)
 			local l_received_pain = math.random( ( l_Enemy.properties.strong/ 2 ), l_Enemy.properties.strong) 
-			local l_received_pain_to_hit = l_Enemy.properties.strong * 0.9		-- Busco el 80% de la fuerza total
+			local l_received_pain_to_hit = l_Enemy.properties.strong * 0.95		-- Busco el 95% de la fuerza total
 			print_logger(1, "l_received_pain_to_hit----- :"..l_received_pain_to_hit)
 			print_logger(1, "l_received_pain----- :"..l_received_pain)
 			if ( l_received_pain >= l_received_pain_to_hit  ) then
