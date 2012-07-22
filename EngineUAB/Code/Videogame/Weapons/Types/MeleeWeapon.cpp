391d9ee6bdf3179359e50feda05b8f202484b231
mcuscullola <mcuscullola@gmail.com>
2012-07-22 16:14:04 +0000

    Añadida rotación de las armas según el hueso asignado.

--- EngineUAB/Code/Videogame/Weapons/Types/MeleeWeapon.cpp
+++ EngineUAB/Code/Videogame/Weapons/Types/MeleeWeapon.cpp
@@ -21,34 +21,44 @@ void CMeleeWeapon::Update(float _ElapsedTime)
 {
 	Vect3f l_TranslationLeft		= v3fZERO;
 	Vect3f l_TranslationRight		= v3fZERO;
+	Vect4f l_RotationLeft			= v3fZERO;
+	Vect4f l_RotationRight			= v3fZERO;
 	Mat44f l_TransformMatrix		= m44fIDENTITY;
+	Mat44f l_RotationMatrix			= m44fIDENTITY;
 	Mat44f l_AnimatedModelTransform = m_pAnimatedModel->GetTransform();
 
 	if( m_WeaponHandType == LEFT_HAND || m_WeaponHandType == BOTH_HAND )
 	{
 		m_pAnimatedModel->GetBonePosition(m_LeftHandName, l_TranslationLeft);
-		l_TransformMatrix.Translate(l_TranslationLeft);
+		m_pAnimatedModel->GetBoneRotation(m_LeftHandName, l_RotationLeft);
 
-		l_TransformMatrix = l_AnimatedModelTransform * l_TransformMatrix;
+		l_TransformMatrix.Translate(l_TranslationLeft);
+		l_RotationMatrix.SetFromQuaternion(l_RotationLeft);
+
+		l_TransformMatrix = l_AnimatedModelTransform * l_TransformMatrix * l_RotationMatrix;
 
 		m_pWeaponLeft->SetPosition( l_TransformMatrix.GetPos() );
-		m_pWeaponLeft->SetYaw( l_TransformMatrix.GetYaw() );
-		m_pWeaponLeft->SetPitch( l_TransformMatrix.GetPitch() );
-		m_pWeaponLeft->SetRoll( l_TransformMatrix.GetRoll() );
+		m_pWeaponLeft->SetYaw(  mathUtils::Rad2Deg(l_TransformMatrix.GetYaw()) );
+		m_pWeaponLeft->SetPitch( mathUtils::Rad2Deg(l_TransformMatrix.GetPitch()) );
+		m_pWeaponLeft->SetRoll( mathUtils::Rad2Deg(l_TransformMatrix.GetRoll()) );
 	}
 
 	if( m_WeaponHandType == RIGHT_HAND || m_WeaponHandType == BOTH_HAND )
 	{
 		l_TransformMatrix.SetIdentity();
+		l_RotationMatrix.SetIdentity();
 
 		m_pAnimatedModel->GetBonePosition(m_RightHandName, l_TranslationRight);
-		l_TransformMatrix.Translate(l_TranslationRight);
+		m_pAnimatedModel->GetBoneRotation(m_RightHandName, l_RotationRight);
 
-		l_TransformMatrix = l_AnimatedModelTransform * l_TransformMatrix;
+		l_TransformMatrix.Translate(l_TranslationRight);
+		l_RotationMatrix.SetFromQuaternion(l_RotationRight);
+
+		l_TransformMatrix = l_AnimatedModelTransform * l_TransformMatrix * l_RotationMatrix;
 
 		m_pWeaponRight->SetPosition( l_TransformMatrix.GetPos() );
-		m_pWeaponRight->SetYaw( l_TransformMatrix.GetYaw() );
-		m_pWeaponRight->SetPitch( l_TransformMatrix.GetPitch() );
-		m_pWeaponRight->SetRoll( l_TransformMatrix.GetRoll() );
+		m_pWeaponRight->SetYaw( mathUtils::Rad2Deg(l_TransformMatrix.GetYaw()) );
+		m_pWeaponRight->SetPitch( mathUtils::Rad2Deg(l_TransformMatrix.GetPitch()) );
+		m_pWeaponRight->SetRoll( mathUtils::Rad2Deg(l_TransformMatrix.GetRoll()) );
 	}
 }
