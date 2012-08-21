7ed8fa487b4715fe7efa96da2e28a7c6197fe587
mcuscullola <mcuscullola@gmail.com>
2012-08-21 17:38:53 +0000

    Suavizado al girar Caperucita (sólo direcciones de adelante y laterales; falta atrás)

--- EngineUAB/Run/Videogame/Data/Scripts/Game/Player/player_run_state.lua
+++ EngineUAB/Run/Videogame/Data/Scripts/Game/Player/player_run_state.lua
@@ -2,6 +2,7 @@ class 'CPlayerRunState' (CState)
 	function CPlayerRunState:__init(name) 
 		CState.__init(self, name)
 		self.action_2_input = core:get_action_to_input()
+		self.rotation_velocity = 1.0
 	end
 
 	function CPlayerRunState:OnEnter(_CCharacter)
@@ -11,11 +12,13 @@ class 'CPlayerRunState' (CState)
 	end
 	
 	function CPlayerRunState:Execute(_CCharacter)
-		local l_d = 0.0
-		local l_yaw = 0.0
-		local l_dir = Vect3f(0.0, 0.0, 0.0)
-		local l_move_player = false
-		local enemy_detected = nil
+		local l_d 				= 0.0
+		local l_yaw 			= 0.0
+		local l_dir 			= Vect3f(0.0, 0.0, 0.0)
+		local l_move_player 	= false
+		local enemy_detected 	= nil
+		local elapsed_time		= _CCharacter.elapsed_time
+		local l_model_yaw 		= _CCharacter.physic_controller.yaw
 		
 		--Miramos si hay un target a poder fijarse
 		if not _CCharacter.is_target_fixed then
@@ -36,64 +39,127 @@ class 'CPlayerRunState' (CState)
 			
 			--Actualización del yaw a partir del movimiento del mouse
 			l_d = self.action_2_input:do_action_mouse('YawPlayer')
-			_CCharacter.yaw = _CCharacter.yaw - l_d
-			_CCharacter.yaw = angle_filter(_CCharacter.yaw)
-			l_yaw = _CCharacter.yaw
+			_CCharacter.yaw 	= _CCharacter.yaw - l_d
+			_CCharacter.yaw 	= angle_filter(_CCharacter.yaw)
+			l_yaw 				= _CCharacter.yaw
+		
+			--Guarda la dirección anterior
+			_CCharacter.previous_direction = _CCharacter.current_direction
 		
 			--Mira los controles de movimiento
 			if self.action_2_input:do_action('MovePlayerUp') then --El player se mueve hacia adelante
 				if self.action_2_input:do_action('MovePlayerLeft') then
 					l_yaw = l_yaw + math.pi/4
+					
+					if math.abs(l_model_yaw - l_yaw) > 0.1 * self.rotation_velocity then
+						if l_yaw >= l_model_yaw then
+							l_model_yaw = l_model_yaw + math.pi/4 * self.rotation_velocity * elapsed_time
+						else
+							l_model_yaw = l_model_yaw - math.pi/4 * self.rotation_velocity * elapsed_time
+						end
+					end
+
 				elseif self.action_2_input:do_action('MovePlayerRight') then
 					l_yaw = l_yaw - math.pi/4
+					
+					if math.abs(l_model_yaw - l_yaw) > 0.1 * self.rotation_velocity then
+						if l_yaw >= l_model_yaw then
+							l_model_yaw = l_model_yaw + math.pi/4 * self.rotation_velocity * elapsed_time
+						else
+							l_model_yaw = l_model_yaw - math.pi/4 * self.rotation_velocity * elapsed_time
+						end
+					end
+				else
+					if math.abs(l_model_yaw - l_yaw) > 0.1 * self.rotation_velocity then
+						if l_yaw >= l_model_yaw then
+							l_model_yaw = l_model_yaw + self.rotation_velocity * elapsed_time
+						else
+							l_model_yaw = l_model_yaw - self.rotation_velocity * elapsed_time
+						end
+					end
 				end
+				
 				l_dir = Vect3f(math.cos(l_yaw), 0.0, math.sin(l_yaw))
 				l_move_player = true
 				
 			elseif self.action_2_input:do_action('MovePlayerDown') then --El player se mueve hacia atrás
 				l_yaw = l_yaw - math.pi
+				--l_model_yaw = l_model_yaw - math.pi
+				
 				if self.action_2_input:do_action('MovePlayerLeft') then
 					l_yaw = l_yaw - math.pi/4
+					l_model_yaw = l_model_yaw - math.pi/4 * self.rotation_velocity * elapsed_tim
+					--TODO: Corrección de dirección
+					
 				elseif self.action_2_input:do_action('MovePlayerRight') then
 					l_yaw = l_yaw + math.pi/4
+					l_model_yaw = l_model_yaw + math.pi/4 * self.rotation_velocity * elapsed_time
+					--TODO: Corrección de dirección
 				end
+				
 				l_dir = Vect3f(math.cos(l_yaw), 0.0, math.sin(l_yaw))
 				l_move_player = true
 				
 			elseif self.action_2_input:do_action('MovePlayerLeft') then --El player se mueve hacia la izquierda
 				l_yaw = l_yaw + math.pi/2
+				
+				if math.abs(l_model_yaw - l_yaw) > 0.1 * self.rotation_velocity then
+					if l_yaw <= l_model_yaw then
+						l_model_yaw = l_model_yaw - math.pi/2 * self.rotation_velocity * elapsed_time
+					else
+						l_model_yaw = l_model_yaw + math.pi/2 * self.rotation_velocity * elapsed_time
+					end
+				end
+				
 				l_dir = Vect3f(math.cos(l_yaw), 0.0, math.sin(l_yaw))
 				l_move_player = true
 				
 			elseif self.action_2_input:do_action('MovePlayerRight') then --El player se mueve hacia la derecha
 				l_yaw = l_yaw - math.pi/2
+				
+				if math.abs(l_model_yaw - l_yaw) > 0.1 * self.rotation_velocity then
+					if l_yaw <= l_model_yaw then
+						l_model_yaw = l_model_yaw - math.pi/2 * self.rotation_velocity * elapsed_time
+					else
+						l_model_yaw = l_model_yaw + math.pi/2 * self.rotation_velocity * elapsed_time
+					end
+				end
+				
 				l_dir = Vect3f(math.cos(l_yaw), 0.0, math.sin(l_yaw))
 				l_move_player = true
 			end
+		end
+		
+		if l_model_yaw > 2*math.pi then
+			l_model_yaw = l_model_yaw - 2*math.pi
+		elseif l_model_yaw < -2*math.pi then
+			l_model_yaw = l_model_yaw + 2*math.pi
+		end
+		
+		if math.abs(l_model_yaw - l_yaw) < 0.1 * self.rotation_velocity then
+			l_model_yaw = l_yaw
 		end
 		
 		--Le aplica la velocidad al movimiento
 		l_dir = l_dir * 10.0 * _CCharacter.elapsed_time
 		
 		--Mueve el controller físico
-		_CCharacter.physic_controller:move(l_dir, _CCharacter.elapsed_time)
+		--_CCharacter.physic_controller:move(l_dir, _CCharacter.elapsed_time)
 		
 		if l_move_player then
 			--Pone de forma correcta los ángulos
-			_CCharacter.physic_controller.yaw = l_yaw
-			_CCharacter.animated_model.yaw = -(l_yaw * 180.0 / math.pi) + 90.0
+			_CCharacter.physic_controller.yaw = l_model_yaw
+			_CCharacter.animated_model.yaw = -(l_model_yaw * 180.0 / math.pi) + 90.0
 			
 			--Mira si se hace un salto
 			if self.action_2_input:do_action('PlayerJump') then
 				_CCharacter.logic_fsm:change_state(_CCharacter.jump)
 				_CCharacter.graphic_fsm:change_state(_CCharacter.animated_jump)
-				print_logger(0, "salta")
 			end
 		else
 			--Cambia de estado a idle
 			_CCharacter.logic_fsm:change_state(_CCharacter.idle)
 			_CCharacter.graphic_fsm:change_state(_CCharacter.animated_idle)
-			print_logger(0, "idle")
 		end
 	end
 	
