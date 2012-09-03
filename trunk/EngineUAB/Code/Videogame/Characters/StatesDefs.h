bd0e18f0934bda2092e8b3684abc704550bd2e7d
jordi.arenas.vigo <jordi.arenas.vigo@38b49c57-6b6f-6c24-9f89-8eb2c450b9d7>
2012-09-03 00:59:44 +0000

    Nuevo fichero de definiciones para estados

---
+++ trunk/EngineUAB/Code/Videogame/Characters/StatesDefs.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#ifndef __STATES_DEFS_H__
+#define __STATES_DEFS_H__
+
+
+// ********* ESTADOS ANIMADOS *********** 
+#define IDLE_STATE				"idle"
+#define IDLE2_STATE				"idle2"
+#define RUN_STATE				"run"
+#define DEATH_STATE				"death"
+#define DEFENSE_STATE			"defense"
+#define HIT_STATE				"hit"
+#define JUMP_STATE				"jump"
+#define RIGHT_LATERAL_STATE		"right_lateral"
+#define LEFT_LATERAL_STATE		"left_lateral"
+#define STILL_ATTACK_STATE		"attack_1"
+#define RUN_ATTACK_STATE		"attack_2"
+#define WALK_STATE				"walk"
+
+#endif __STATES_DEFS_H__
