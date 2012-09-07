21dd6f615eb00edfc3e10eb0e693a232e6dbec80
jordi.arenas.vigo <jordi.arenas.vigo@38b49c57-6b6f-6c24-9f89-8eb2c450b9d7>
2012-09-07 14:13:20 +0000

    done Fatigued function

--- EngineUAB/Code/Videogame/Characters/Enemies/Rabbit/Rabbit.cpp
+++ EngineUAB/Code/Videogame/Characters/Enemies/Rabbit/Rabbit.cpp
@@ -231,15 +231,6 @@ void CRabbit::LoadLogicStates( void )
 // -----------------------------------------
 bool CRabbit::IsFatigued( void )
 {
-	// print_logger ( 1, "hits done : ".._CCharacter.hits_done.." and hits let : ".._CCharacter.total_hits_done_to_tired)
-	if ( m_HitsDone == m_TotalHitsDoneToTired ) 
-	{
-		m_HitsDone = 0;
-		return true;
-	}	
-	else 
-	{
-		return false;
-	}
+	return m_HitsDone == m_TotalHitsDoneToTired;
 }
 
