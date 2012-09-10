ba3f210d874ca38186a0aa40fe03415051dc703c
jordi.arenas.vigo <jordi.arenas.vigo@38b49c57-6b6f-6c24-9f89-8eb2c450b9d7>
2012-09-10 20:48:44 +0000

    Cambio nombre funcion frustum

--- EngineUAB/Code/Graphics/Commands/RenderDebugPhysicsSceneRendererCommand.cpp
+++ EngineUAB/Code/Graphics/Commands/RenderDebugPhysicsSceneRendererCommand.cpp
@@ -15,17 +15,17 @@
 CRenderDebugPhysicsSceneRendererCommand::CRenderDebugPhysicsSceneRendererCommand(CXMLTreeNode &Node)
 	: CSceneRendererCommand ( Node )
 {
-	// <render_debug_physics active="true" draw_fronts="true" draw_frustrum="true" />
+	// <render_debug_physics active="true" draw_fronts="true" draw_frustum="true" />
 	bool active = Node.GetBoolProperty("active", false);
 	SetActive(active);
 
 	m_DrawFront		= Node.GetBoolProperty("draw_fronts", false);
-	m_DrawFrustrum	= Node.GetBoolProperty("draw_fustrum", false);
+	m_Drawfrustum	= Node.GetBoolProperty("draw_frustum", false);
 	m_DrawNames		= Node.GetBoolProperty("draw_names", false);
 	m_DrawRays		= Node.GetBoolProperty("draw_rays", false);
 	
 	CORE->GetPhysicsManager()->SetDrawFront( Node.GetBoolProperty("draw_fronts", false) );
-	CORE->GetPhysicsManager()->SetDrawFrustrum( Node.GetBoolProperty("draw_frustrum", false) );
+	CORE->GetPhysicsManager()->SetDrawfrustum( Node.GetBoolProperty("draw_frustum", false) );
 	CORE->GetPhysicsManager()->SetDrawNames( Node.GetBoolProperty("draw_names", false) );
 	CORE->GetPhysicsManager()->SetDrawRays( Node.GetBoolProperty("draw_rays", false) );
 }
