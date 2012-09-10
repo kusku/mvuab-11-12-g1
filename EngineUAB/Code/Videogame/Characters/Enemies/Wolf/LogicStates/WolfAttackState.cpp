7a69861f78e2244403f4eb5f8e214dd9f05db8d7
jordi.arenas.vigo <jordi.arenas.vigo@38b49c57-6b6f-6c24-9f89-8eb2c450b9d7>
2012-09-10 21:15:27 +0000

    frustum

--- EngineUAB/Code/Videogame/Characters/Enemies/Wolf/LogicStates/WolfAttackState.cpp
+++ EngineUAB/Code/Videogame/Characters/Enemies/Wolf/LogicStates/WolfAttackState.cpp
@@ -127,7 +127,7 @@ void CWolfAttackState::Execute( CCharacter* _Character, float _ElapsedTime )
 			else if ( l_ActiveActionState == "go_in_to_fustrum" ) 
 			{
 				float l_Angle = 22.f;		// 22,5 graus de fustrum
-				m_pWolf->GoInToFrustrum(l_Angle, _ElapsedTime);
+				m_pWolf->GoInTofrustum(l_Angle, _ElapsedTime);
 			}
 
 			#if defined _DEBUG
