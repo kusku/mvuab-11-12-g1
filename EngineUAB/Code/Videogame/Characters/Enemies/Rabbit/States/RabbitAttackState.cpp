86f27477bbfbfa6022b85849c9f23f107771ba6e
jordi.arenas.vigo <jordi.arenas.vigo@38b49c57-6b6f-6c24-9f89-8eb2c450b9d7>
2012-09-11 23:55:00 +0000

    falta ortográfica....

--- EngineUAB/Code/Videogame/Characters/Enemies/Rabbit/States/RabbitAttackState.cpp
+++ EngineUAB/Code/Videogame/Characters/Enemies/Rabbit/States/RabbitAttackState.cpp
@@ -130,7 +130,7 @@ void CRabbitAttackState::Execute( CCharacter* _Character, float _ElapsedTime )
 			else if ( l_ActiveActionState == "go_in_to_fustrum" ) 
 			{
 				float l_Angle = 22.f;		// 22,5 graus de fustrum
-				m_pRabbit->GoInToFrustrum(l_Angle, _ElapsedTime);
+				m_pRabbit->GoInTofrustum(l_Angle, _ElapsedTime);
 			}
 
 			#if defined _DEBUG
