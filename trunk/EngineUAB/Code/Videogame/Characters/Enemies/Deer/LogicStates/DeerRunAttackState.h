38cbc04a6ec53b3bbc998bc927d6926d3778949b
jordi.arenas.vigo <jordi.arenas.vigo@38b49c57-6b6f-6c24-9f89-8eb2c450b9d7>
2012-09-11 22:57:40 +0000

    Run Attack del Deer más rápido. Parecido al Charger de L4D2

--- trunk/EngineUAB/Code/Videogame/Characters/Enemies/Deer/LogicStates/DeerRunAttackState.h
+++ trunk/EngineUAB/Code/Videogame/Characters/Enemies/Deer/LogicStates/DeerRunAttackState.h
@@ -5,6 +5,7 @@
 
 
 #include <string>
+#include "Math\Vector3.h"
 #include "StatesMachine\State.h"
 #include "Characters\Character.h"
 
@@ -41,12 +42,16 @@ private:
 	CDeer				*	m_pDeer;
 	CAnimationCallback	*	m_pAnimationCallback;
 
-	float					m_OldMaxSpeed;			// Permite almacenar la vieja velocidad para posteriormente recuperarla
-	float					m_OldMass;				// Permite almacenar la vieja masa para posteriormente recuperarla
-	float					m_CurrentDistance;		// Permite alamacenar la distancia para saber si luego nos pasamos
+	float					m_OldMaxSpeed;				// Permite almacenar la vieja velocidad para posteriormente recuperarla
+	float					m_OldMass;					// Permite almacenar la vieja masa para posteriormente recuperarla
+	float					m_CurrentDistance;			// Permite alamacenar la distancia para saber si luego nos pasamos
+	Vect3f					m_PlayerInitialPosition;	// Permite saber la distancia de inicio de ataque del player. Así sabremos si hemos pasado de largo.
+	Vect3f					m_FailImpactFinalPosition;	// Permite saber la posición final de toda la animación en caso de que el player esquive el ataque
 
 	float					m_CurrentDuration;
 	float					m_AnimationDuration;
+	
+	bool					m_PlayerReached;
 
 };
 
