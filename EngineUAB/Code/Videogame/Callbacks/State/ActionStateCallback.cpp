a579c2bacdeb23bb0946ed40cef753e198775d25
jordi.arenas.vigo <jordi.arenas.vigo@38b49c57-6b6f-6c24-9f89-8eb2c450b9d7>
2012-09-12 23:31:17 +0000

    Mejoras en la clase con más opciones de intervalos y valores minimos

--- EngineUAB/Code/Videogame/Callbacks/State/ActionStateCallback.cpp
+++ EngineUAB/Code/Videogame/Callbacks/State/ActionStateCallback.cpp
@@ -5,28 +5,58 @@
 	#include "Memory\MemLeaks.h"
 #endif
 
-CActionStateCallback::CActionStateCallback( float _MinInterval, float _MaxInterval )
-	: m_MinTime			(_MinInterval)
-	, m_MaxTime			(_MaxInterval)
+CActionStateCallback::CActionStateCallback( float _MinValue, float _MaxValue )
+	: m_MinTime			(_MinValue)
+	, m_MaxTime			(_MaxValue)
+	, m_CurrentTime		(_MinValue)
 	, m_ActionStarted	(false)
 {
 }
 
+CActionStateCallback::CActionStateCallback( float _MinValue, float _MinIntervalMaxValue, float _MaxIntervalMaxValue )
+	: m_MinTime			(_MinValue)
+	, m_MaxTime			(0.f)
+	, m_CurrentTime		(_MinValue)
+	, m_ActionStarted	(false)
+{
+	m_MaxTime = GetRandomAnimationTime(_MinIntervalMaxValue, _MaxIntervalMaxValue);
+}
 
 CActionStateCallback::~CActionStateCallback( void )
 {
 }
 
+void CActionStateCallback::InitAction( void )														
+{ 
+	m_ActionStarted = false; 
+}
+
+void CActionStateCallback::InitAction( float _MinValue, float _MaxValue )		
+{ 
+	m_ActionStarted = false; 
+	m_MinTime = _MinValue; 
+	m_MaxTime = _MaxValue; 
+	m_CurrentTime = _MinValue; 
+}
+
+void CActionStateCallback::InitAction( float _MinValue, float _MinIntervalMaxValue, float _MaxIntervalMaxValue )		
+{ 
+	m_ActionStarted = false; 
+	m_MinTime = _MinValue; 
+	m_MaxTime = GetRandomAnimationTime(_MinIntervalMaxValue, _MaxIntervalMaxValue); 
+	m_CurrentTime = _MinValue; 
+}
+
+
 void CActionStateCallback::StartAction( void )
 {
-	m_TotalActionTime = GetRandomAnimationTime();
-	m_CurrentTime = 0.f;
+	m_CurrentTime = m_MinTime;
 	m_ActionStarted = true;
 }
 
 bool CActionStateCallback::IsActionFinished( void )			
 { 
-	if ( m_CurrentTime >= m_TotalActionTime )
+	if ( m_CurrentTime >= m_MaxTime )
 	{
 		m_ActionStarted = false;
 		return true;
@@ -37,7 +67,7 @@ bool CActionStateCallback::IsActionFinished( void )
 	}
 }
 
-float CActionStateCallback::GetRandomAnimationTime( void )
+float CActionStateCallback::GetRandomAnimationTime( float _MinInterval, float _MaxInterval )
 {
-	return BoostRandomHelper::GetFloat(m_MinTime, m_MaxTime);
+	return BoostRandomHelper::GetFloat(_MinInterval, _MaxInterval);
 }
--- EngineUAB/Code/Videogame/Callbacks/State/ActionStateCallback.h
+++ EngineUAB/Code/Videogame/Callbacks/State/ActionStateCallback.h
@@ -7,34 +7,36 @@ class CActionStateCallback
 {
 public:
 	// ------------- Constructors i Destructors ----------------
-				CActionStateCallback	( float _MinInterval, float _MaxInterval );
+				CActionStateCallback	( float _MinValue, float _MaxValue );
+				CActionStateCallback	( float _MinValue, float _MinIntervalMaxValue, float _MaxIntervalMaxValue );
 				~CActionStateCallback	( void );
 
 	// ------------- Mètodes Principals ------------------------
-	void		InitAction				( void )				{ m_ActionStarted = false; }
+	void		InitAction				( void );
+	void		InitAction				( float _MinValue, float _MaxValue);
+	void		InitAction				( float _MinValue, float _MinInterval, float _MaxInterval );
+	
 	void		StartAction				( void );
+	
 	bool		IsActionFinished		( void );
-	bool		IsActionStarted			( void ) const			{ return m_ActionStarted; }
-	bool		IsActionInTime			( float _TimeToCheck )	{ 
-																	return m_CurrentTime >= _TimeToCheck; 
-																}
+	bool		IsActionStarted			( void ) const													{ return m_ActionStarted; }
+	bool		IsActionInTime			( float _TimeToCheck )											{ return m_CurrentTime >= _TimeToCheck; }
 
-	void		Update					( float _ElapsedTime )	{ m_CurrentTime += _ElapsedTime; }
+	void		Update					( float _ElapsedTime )											{ m_CurrentTime += _ElapsedTime; }
 
 	// ------------- Mètodes -----------------------------------
-	void		SetTimeRange			( float _MinInterval, float _MaxInterval )		{ m_MinTime = _MinInterval; m_MaxTime = _MaxInterval; }
+	void		SetTimeRange			( float _MinValue, float _MaxValue )							{ m_MinTime = _MinValue; m_MaxTime = _MaxValue; }
 
 	// ------------- Properties---------------------------------
 	
 
 private:
-	float		GetRandomAnimationTime	( void );
+	float		GetRandomAnimationTime	( float _MinInterval, float _MaxInterval );
 	
 	// ------------- Members -----------------------------------
 	float		m_MinTime;
 	float		m_MaxTime;
 
-	float		m_TotalActionTime;
 	float		m_CurrentTime;
 	bool		m_ActionStarted;
 };
