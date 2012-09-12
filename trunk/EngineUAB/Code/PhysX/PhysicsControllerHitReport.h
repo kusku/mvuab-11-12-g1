0399467c258e51e7eef083ea968831f0072babb9
jordi.arenas.vigo <jordi.arenas.vigo@38b49c57-6b6f-6c24-9f89-8eb2c450b9d7>
2012-09-12 23:46:34 +0000

    Codigo con algunos test para ver si llegaba...

--- trunk/EngineUAB/Code/PhysX/PhysicsControllerHitReport.h
+++ trunk/EngineUAB/Code/PhysX/PhysicsControllerHitReport.h
@@ -3,6 +3,9 @@
 #ifndef _PHYSICS_CONTROLLER_HIT_REPORT_H
 #define _PHYSICS_CONTROLLER_HIT_REPORT_H
 
+#include "Logger\Logger.h"
+#include "Base.h"
+
 class CPhysicsControllerHitReport : public NxUserControllerHitReport
 {
 	public:
@@ -22,6 +25,22 @@ class CPhysicsControllerHitReport : public NxUserControllerHitReport
 					actor.addForceAtLocalPos(hit.dir*coeff, NxVec3(0,0,0), NX_IMPULSE);
 				}
 			}
+
+			if ( group == ECG_PLAYER )
+			{
+				#if defined _DEBUG
+					LOGGER->AddNewLog( ELL_INFORMATION, "onShapeHit -> Hit ECG_PLAYER" );
+				#endif	
+				return NX_ACTION_NONE;
+			}
+
+			if ( group == ECG_ENEMY )
+			{
+				#if defined _DEBUG
+					LOGGER->AddNewLog( ELL_INFORMATION, "onShapeHit -> Hit ECG_ENEMY" );
+				#endif	
+				return NX_ACTION_NONE;
+			}
 		}
 
 		return NX_ACTION_NONE;
@@ -29,6 +48,34 @@ class CPhysicsControllerHitReport : public NxUserControllerHitReport
 
 	virtual NxControllerAction onControllerHit(const NxControllersHit& hit)
 	{
+		if(hit.controller)
+		{
+			NxCollisionGroup group = hit.controller->getActor()->getGroup();
+			if ( group == ECG_DYNAMIC_OBJECTS )
+			{
+				#if defined _DEBUG
+					LOGGER->AddNewLog( ELL_INFORMATION, "onControllerHit -> Hit ECG_DYNAMIC_OBJECTS" );
+				#endif	
+				return NX_ACTION_NONE;
+			}
+
+			if ( group == ECG_PLAYER )
+			{
+				#if defined _DEBUG
+					LOGGER->AddNewLog( ELL_INFORMATION, "onControllerHit -> Hit ECG_PLAYER" );
+				#endif	
+				return NX_ACTION_NONE;
+			}
+
+			if ( group == ECG_ENEMY )
+			{
+				#if defined _DEBUG
+					LOGGER->AddNewLog( ELL_INFORMATION, "onControllerHit -> Hit ECG_ENEMY" );
+				#endif	
+				return NX_ACTION_NONE;
+			}
+		}
+
 		return NX_ACTION_NONE;
 	}
 };
