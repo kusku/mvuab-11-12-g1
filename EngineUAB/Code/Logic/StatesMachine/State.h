7a0216ba08aa01938066519a51cae46cfa928612
jordi.arenas.vigo <jordi.arenas.vigo@38b49c57-6b6f-6c24-9f89-8eb2c450b9d7>
2012-09-21 16:03:53 +0000

    Mètodos de partículas puestos en base para los estados

--- EngineUAB/Code/Logic/StatesMachine/State.h
+++ EngineUAB/Code/Logic/StatesMachine/State.h
@@ -3,11 +3,18 @@
 #ifndef __CLASS_STATE_H__
 #define __CLASS_STATE_H__
 
+#include <string>
 #include "MessageDefs.h"
 #include "Particles\ParticleEmitterManager.h"
+#include "Particles\ParticleEmitterInstance.h"
+#include "Particles\ParticleEmitterSystemManager.h"
+
+//#include "mathutils\Mat44f"
+
+#include "Core.h"
 
 //--- Foward Declarations ---//
-class CParticleEmitter;
+class CCharacter;
 //---------------------------//
 
 struct STelegram;
@@ -20,7 +27,7 @@ class CState : public CNamed
 {
 public:
 	// ------------- Constructors i Destructors ----------------
-					CState		( T* )							{ return; }
+					CState		( T* )								{ return; }
 					CState		( T* , const std::string &_Name )	{ SetName(_Name); }
 	virtual			~CState		( void ) {};
 
@@ -52,10 +59,45 @@ public:
 															return true; 
 														};
 	
-	//----Properties ------------------------------------------
-	CParticleEmitter*	GetParticleEmitter( const std::string & _EmitterName ) const { return CORE->GetParticleEmitterManager()->GetResource(_EmitterName); }
+	//----Methods --------------------------------------------
+	void SetParticlePosition( CCharacter* _pCharacter, const std::string &_ParticlesCoreName, const std::string &_ParticlesInstaceName, const std::string &_Bone = "", const Vect3f &_Position = Vect3f(0,0,0))
+	{
+		if ( _Bone.compare( "" ) != 0 )
+		{
+			CAnimatedInstanceModel *l_pAnimatedModel = _pCharacter->GetAnimatedModel();
+	
+			Mat44f l_TransformMatrix		= m44fIDENTITY;
+			Mat44f l_RotationMatrix			= m44fIDENTITY;
+			Vect4f l_Rotation				= v3fZERO;
+			Vect3f l_Translation			= v3fZERO;
+			Mat44f l_AnimatedModelTransform = l_pAnimatedModel->GetTransform();
+	
+			l_pAnimatedModel->GetBonePosition(_Bone, l_Translation);
+			l_pAnimatedModel->GetBoneRotation(_Bone, l_Rotation);
+	
+			l_TransformMatrix.Translate(l_Translation);
+			l_RotationMatrix.SetFromQuaternion(l_Rotation);
+	
+			l_TransformMatrix = l_AnimatedModelTransform * l_TransformMatrix * l_RotationMatrix;
+	
+			GetParticleEmitterInstance(_ParticlesCoreName, _ParticlesInstaceName)->SetPosition( l_TransformMatrix.GetPos() );
+		}
+		else 
+		{
+			GetParticleEmitterInstance(_ParticlesCoreName, _ParticlesInstaceName)->SetPosition( _Position );
+		}
+	}
 
+	//----Properties -----------------------------------------
+	CParticleEmitterInstance* GetParticleEmitterInstance( const std::string & _EmitterCoreName, const std::string & _EmitterInstanceName ) const 
+	{ 
+		return CORE->GetParticleEmitterManager()->GetResource(_EmitterCoreName)->GetParticleEmitterInstance(_EmitterInstanceName); 
+	}
 
+	CParticleEmitterSystemInstance* GetParticleGroupInstance( const std::string & _EmitterCoreGroupName, const std::string & _EmitterInstanceGroupName ) const 
+	{ 
+		return CORE->GetParticleEmitterSystemManager()->GetResource(_EmitterCoreGroupName)->GetParticleEmitterSystemInstance(_EmitterInstanceGroupName); 
+	}
 	// ------------- Members ---------------------------------
 private:
 
