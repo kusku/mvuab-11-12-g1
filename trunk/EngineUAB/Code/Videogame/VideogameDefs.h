7298290d9f5913c9bd1ac8bb5393882a5e74d92a
mcuscullola@gmail.com <mcuscullola@gmail.com@38b49c57-6b6f-6c24-9f89-8eb2c450b9d7>
2012-09-23 11:55:23 +0000

    Añadida constante de aceleración.

--- trunk/EngineUAB/Code/Videogame/VideogameDefs.h
+++ trunk/EngineUAB/Code/Videogame/VideogameDefs.h
@@ -12,6 +12,7 @@
 const float VELOCIDAD_MINIMA	= 1.0f;
 const float VELOCIDAD_MAXIMA	= 3.0f;
 const float FUERZA_MAXIMA		= 0.15f;
+const float ACELERACION_MINIMA	= 1.f;
 const float ACELERACION_MAXIMA	= 2.f;
 const float ROTACION_MAXIMA		= 360.f;
 
