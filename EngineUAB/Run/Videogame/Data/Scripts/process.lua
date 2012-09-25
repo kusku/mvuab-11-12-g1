8e82bd74bfd4f5c2b37772540cd940f493d189bd
mcuscullola <mcuscullola@gmail.com>
2012-09-25 18:13:58 +0000

    Modificado el sistema de finalización del juego.

--- EngineUAB/Run/Videogame/Data/Scripts/process.lua
+++ EngineUAB/Run/Videogame/Data/Scripts/process.lua
@@ -1,16 +1,20 @@
 function change_to_game_process()
 	core:get_process():load_game_data()
-	--core:get_process():change_process()
 end
 
 function change_to_game_over_gui_process()
-	get_game_process():change_to_gui_process()
+	core:set_game_mode(false)
 	core:get_gui_manager():pop_windows()
 	core:get_gui_manager():push_windows('GameOver')
 end
 
-function change_to_end_gui_process()
+function change_to_gui_process()
 	get_game_process():change_to_gui_process()
+	core:get_gui_manager():pop_windows()
+end
+
+function change_to_end_gui_process()
+	core:set_game_mode(false)
 	core:get_gui_manager():pop_windows()
 	core:get_gui_manager():push_windows('End')
 end
