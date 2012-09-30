b802ee7210bdab224aa968f48fdf2d66a0aa1302
jordi.arenas.vigo <jordi.arenas.vigo@38b49c57-6b6f-6c24-9f89-8eb2c450b9d7>
2012-09-30 14:12:45 +0000

    script de zona de combate 3

--- EngineUAB/Run/Videogame/Data/Scripts/Game/Triggers/combat_zone_3.lua
+++ EngineUAB/Run/Videogame/Data/Scripts/Game/Triggers/combat_zone_3.lua
@@ -1,15 +1,23 @@
+loadfile "utils.lua"
+
 class 'CCombatZone3Trigger' (CBoxTrigger)
 	function CCombatZone3Trigger:__init()
 		CBoxTrigger.__init(self)
 
-		--ActionState = CActionStateCallback(0,1)
+		--self.action_time = CActionStateCallback(0,1)
 		
 	end
 	
 	function CCombatZone3Trigger:update(elapsed_time)
+		--print_logger(0, "CCombatZone3Trigger:update(elapsed_time), ")
+		--print_logger(0, "hit")
+		self.elapsed_time = elapsed_time
 	end
 	
 	function CCombatZone3Trigger:on_enter()
+		--local t = { 'enemy22', 'enemy23', 'enemy24' }
+
+		
 		get_game_process():get_character_manager():get_enemy_by_name('enemy22').enable = true
 		get_game_process():get_character_manager():get_enemy_by_name('enemy23').enable = true
 		get_game_process():get_character_manager():get_enemy_by_name('enemy24').enable = true
