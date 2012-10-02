025a8bb5a802c516dcdcae663fe820aefc18e2d5
jordi.arenas.vigo <jordi.arenas.vigo@38b49c57-6b6f-6c24-9f89-8eb2c450b9d7>
2012-10-02 14:45:21 +0000

    5 triggers actualizados

--- trunk/EngineUAB/Run/Videogame/Data/Scripts/Game/Triggers/combat_zone_4.lua
+++ trunk/EngineUAB/Run/Videogame/Data/Scripts/Game/Triggers/combat_zone_4.lua
@@ -1,40 +1,66 @@
-loadfile "utils.lua"
 
-class 'CCombatZone3Trigger' (CBoxTrigger)
-	function CCombatZone3Trigger:__init()
+class 'CCombatZone4Trigger' (CBoxTrigger)
+	function CCombatZone4Trigger:__init()
 		CBoxTrigger.__init(self)
 
-		--self.action_time = CActionStateCallback(0,1)
-		self.action_time = 0
+		self.action_time = CActionStateCallback(0,1)
+		self.action_time:init_action()
+		
+		self.enable_action_time = CActionStateCallback(0,0.5)
+		self.enable_action_time:init_action()
+		
+		self.enemy_appeared = 1
+		self.total_enemies  = 3 	-- Total de enemigos en la lista
+		self.is_appeared = false
 	end
 	
-	function CCombatZone3Trigger:update(elapsed_time)
-		--print_logger(0, "CCombatZone3Trigger:update(elapsed_time), ")
-		--print_logger(0, "hit")
-		self.action_time = self.action_time + elapsed_time
+	function CCombatZone4Trigger:update(elapsed_time)
+		--print_logger(0, "CCombatZone4Trigger:update -> Actualizacion trigger zona de combate")
 		
+		local t = { "enemy15", "enemy16", "enemy31", "enemy32", "enemy33" }
+		
+		if ( self.action_time:is_action_started() ) then
+			if ( self.action_time:is_action_finished() ) then
+				-- Ahora incializo la parte de la aparición
+				self.enable_action_time:init_action()
+				self.enable_action_time:start_action()
+			else 
+				self.action_time:update(elapsed_time)
+				
+				if ( get_game_process():get_character_manager():get_enemy_by_name(t[self.enemy_appeared]).enable == false ) then
+					get_game_process():get_character_manager():get_enemy_by_name(t[self.enemy_appeared]):appearance()
+					self.enable_action_time:init_action()
+					self.enable_action_time:start_action()
+				end
+			end 
+		end
+
+		if ( self.enable_action_time:is_action_started() ) then
+			if ( self.enable_action_time:is_action_finished() ) then
+				--if ( get_game_process():get_character_manager():get_enemy_by_name(t[self.enemy_appeared]).enable == false ) then
+					get_game_process():get_character_manager():get_enemy_by_name(t[self.enemy_appeared]).enable = true
+					
+					if (self.enemy_appeared == self.total_enemies ) then
+						self.active = false
+					else
+						self.enemy_appeared = self.enemy_appeared + 1
+						self.action_time:init_action()
+						self.action_time:start_action()
+					end 
+				-- end
+			else 
+				self.enable_action_time:update(elapsed_time)
+			end 
+		end
 	end
 	
-	function CCombatZone3Trigger:on_enter()
-		local t = { "enemy22", "enemy23", "enemy24", "enemy25", "enemy26" , "enemy27", "enemy28" }
-		
-		local i = 1
-		for i=1,# t do
-			get_game_process():get_character_manager():get_enemy_by_name(t[i]).enable = true
-		end 
-		
-		-- if ( self.action_time <= i ) then
-			-- get_game_process():get_character_manager():get_enemy_by_name(t[i]).enable = true
-			-- i = i + 1
-		-- end 
-			
-		
-		
-		self.active = false
+	function CCombatZone4Trigger:on_enter()
+		self.action_time:start_action()
+		self.enemy_appeared = 1
 	end
 	
-	function CCombatZone3Trigger:on_stay()
+	function CCombatZone4Trigger:on_stay()
 	end
 	
-	function CCombatZone3Trigger:on_exit()
+	function CCombatZone4Trigger:on_exit()
 	end
