faf8ac4d6966f6e147f78f91d1dc4178b8fe2802
mcuscullola@gmail.com <mcuscullola@gmail.com@38b49c57-6b6f-6c24-9f89-8eb2c450b9d7>
2012-10-03 18:27:22 +0000

    Añadidas nuevas propiedades de ataque para el player.

--- trunk/EngineUAB/Code/Videogame/Characters/Player/LogicStates/PlayerAttack2State.h
+++ trunk/EngineUAB/Code/Videogame/Characters/Player/LogicStates/PlayerAttack2State.h
@@ -39,7 +39,9 @@ private:
 	float						m_fCurrentVelocityMovement;
 	float						m_fAccelerationMovement;
 	float						m_fAttackDistance;
+	float						m_fDetectionDistance;
 	float						m_fAttackAngle;
+	float						m_fDetectionAngle;
 	float						m_fAttackYaw;
 };
 
