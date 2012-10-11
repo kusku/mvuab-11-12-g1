e4a847819ecf9e91c97954f0565d656364694634
chaconc@gmail.com <chaconc@gmail.com@38b49c57-6b6f-6c24-9f89-8eb2c450b9d7>
2012-10-11 23:26:45 +0000

    Agregando Configurador

---
+++ trunk/EngineUAB/Code/Configuracion/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace Configuracion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbTitle = new System.Windows.Forms.Label();
+            this.tConfig = new System.Windows.Forms.TabControl();
+            this.tpVideo = new System.Windows.Forms.TabPage();
+            this.cbMotionBlurEnable = new System.Windows.Forms.CheckBox();
+            this.cbWindowMode = new System.Windows.Forms.CheckBox();
+            this.cbShadowEnable = new System.Windows.Forms.CheckBox();
+            this.lbShadowQuality = new System.Windows.Forms.Label();
+            this.cbShadowQuality = new System.Windows.Forms.ComboBox();
+            this.lbResolution = new System.Windows.Forms.Label();
+            this.cbResolution = new System.Windows.Forms.ComboBox();
+            this.bApply = new System.Windows.Forms.Button();
+            this.bExit = new System.Windows.Forms.Button();
+            this.tConfig.SuspendLayout();
+            this.tpVideo.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lbTitle
+            // 
+            this.lbTitle.AutoSize = true;
+            this.lbTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTitle.ForeColor = System.Drawing.Color.DarkRed;
+            this.lbTitle.Location = new System.Drawing.Point(5, 9);
+            this.lbTitle.Name = "lbTitle";
+            this.lbTitle.Size = new System.Drawing.Size(375, 37);
+            this.lbTitle.TabIndex = 0;
+            this.lbTitle.Text = "Red Forest Configuration";
+            // 
+            // tConfig
+            // 
+            this.tConfig.Controls.Add(this.tpVideo);
+            this.tConfig.Location = new System.Drawing.Point(12, 53);
+            this.tConfig.Name = "tConfig";
+            this.tConfig.SelectedIndex = 0;
+            this.tConfig.Size = new System.Drawing.Size(705, 178);
+            this.tConfig.TabIndex = 1;
+            // 
+            // tpVideo
+            // 
+            this.tpVideo.Controls.Add(this.cbMotionBlurEnable);
+            this.tpVideo.Controls.Add(this.cbWindowMode);
+            this.tpVideo.Controls.Add(this.cbShadowEnable);
+            this.tpVideo.Controls.Add(this.lbShadowQuality);
+            this.tpVideo.Controls.Add(this.cbShadowQuality);
+            this.tpVideo.Controls.Add(this.lbResolution);
+            this.tpVideo.Controls.Add(this.cbResolution);
+            this.tpVideo.Location = new System.Drawing.Point(4, 22);
+            this.tpVideo.Name = "tpVideo";
+            this.tpVideo.Padding = new System.Windows.Forms.Padding(3);
+            this.tpVideo.Size = new System.Drawing.Size(697, 152);
+            this.tpVideo.TabIndex = 0;
+            this.tpVideo.Text = "Video";
+            this.tpVideo.UseVisualStyleBackColor = true;
+            // 
+            // cbMotionBlurEnable
+            // 
+            this.cbMotionBlurEnable.AutoSize = true;
+            this.cbMotionBlurEnable.CheckAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.cbMotionBlurEnable.Location = new System.Drawing.Point(9, 118);
+            this.cbMotionBlurEnable.Name = "cbMotionBlurEnable";
+            this.cbMotionBlurEnable.Size = new System.Drawing.Size(118, 17);
+            this.cbMotionBlurEnable.TabIndex = 7;
+            this.cbMotionBlurEnable.Text = "Motion Blur Enable:";
+            this.cbMotionBlurEnable.UseVisualStyleBackColor = true;
+            // 
+            // cbWindowMode
+            // 
+            this.cbWindowMode.AutoSize = true;
+            this.cbWindowMode.CheckAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.cbWindowMode.Location = new System.Drawing.Point(240, 14);
+            this.cbWindowMode.Name = "cbWindowMode";
+            this.cbWindowMode.Size = new System.Drawing.Size(134, 17);
+            this.cbWindowMode.TabIndex = 6;
+            this.cbWindowMode.Text = "Window Mode Enable:";
+            this.cbWindowMode.UseVisualStyleBackColor = true;
+            // 
+            // cbShadowEnable
+            // 
+            this.cbShadowEnable.AutoSize = true;
+            this.cbShadowEnable.CheckAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.cbShadowEnable.Location = new System.Drawing.Point(6, 53);
+            this.cbShadowEnable.Name = "cbShadowEnable";
+            this.cbShadowEnable.Size = new System.Drawing.Size(104, 17);
+            this.cbShadowEnable.TabIndex = 5;
+            this.cbShadowEnable.Text = "Shadow Enable:";
+            this.cbShadowEnable.UseVisualStyleBackColor = true;
+            // 
+            // lbShadowQuality
+            // 
+            this.lbShadowQuality.AutoSize = true;
+            this.lbShadowQuality.Location = new System.Drawing.Point(6, 79);
+            this.lbShadowQuality.Name = "lbShadowQuality";
+            this.lbShadowQuality.Size = new System.Drawing.Size(84, 13);
+            this.lbShadowQuality.TabIndex = 3;
+            this.lbShadowQuality.Text = "Shadow Quality:";
+            // 
+            // cbShadowQuality
+            // 
+            this.cbShadowQuality.FormattingEnabled = true;
+            this.cbShadowQuality.Items.AddRange(new object[] {
+            "Low",
+            "Medium",
+            "High",
+            "Very High"});
+            this.cbShadowQuality.Location = new System.Drawing.Point(96, 76);
+            this.cbShadowQuality.Name = "cbShadowQuality";
+            this.cbShadowQuality.Size = new System.Drawing.Size(118, 21);
+            this.cbShadowQuality.TabIndex = 2;
+            // 
+            // lbResolution
+            // 
+            this.lbResolution.AutoSize = true;
+            this.lbResolution.Location = new System.Drawing.Point(6, 15);
+            this.lbResolution.Name = "lbResolution";
+            this.lbResolution.Size = new System.Drawing.Size(60, 13);
+            this.lbResolution.TabIndex = 1;
+            this.lbResolution.Text = "Resolution:";
+            // 
+            // cbResolution
+            // 
+            this.cbResolution.FormattingEnabled = true;
+            this.cbResolution.Items.AddRange(new object[] {
+            "640x360",
+            "800x600",
+            "1024x768",
+            "1280x720",
+            "1280x1024",
+            "1920x1080"});
+            this.cbResolution.Location = new System.Drawing.Point(72, 12);
+            this.cbResolution.Name = "cbResolution";
+            this.cbResolution.Size = new System.Drawing.Size(162, 21);
+            this.cbResolution.TabIndex = 0;
+            // 
+            // bApply
+            // 
+            this.bApply.Location = new System.Drawing.Point(557, 237);
+            this.bApply.Name = "bApply";
+            this.bApply.Size = new System.Drawing.Size(75, 23);
+            this.bApply.TabIndex = 2;
+            this.bApply.Text = "Apply";
+            this.bApply.UseVisualStyleBackColor = true;
+            this.bApply.Click += new System.EventHandler(this.bApply_Click);
+            // 
+            // bExit
+            // 
+            this.bExit.Location = new System.Drawing.Point(638, 237);
+            this.bExit.Name = "bExit";
+            this.bExit.Size = new System.Drawing.Size(75, 23);
+            this.bExit.TabIndex = 3;
+            this.bExit.Text = "Exit";
+            this.bExit.UseVisualStyleBackColor = true;
+            this.bExit.Click += new System.EventHandler(this.bExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(729, 273);
+            this.Controls.Add(this.bExit);
+            this.Controls.Add(this.bApply);
+            this.Controls.Add(this.tConfig);
+            this.Controls.Add(this.lbTitle);
+            this.Name = "Form1";
+            this.Text = "Red Forest Configuration";
+            this.tConfig.ResumeLayout(false);
+            this.tpVideo.ResumeLayout(false);
+            this.tpVideo.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbTitle;
+        private System.Windows.Forms.TabControl tConfig;
+        private System.Windows.Forms.TabPage tpVideo;
+        private System.Windows.Forms.Button bApply;
+        private System.Windows.Forms.Button bExit;
+        private System.Windows.Forms.Label lbResolution;
+        private System.Windows.Forms.ComboBox cbResolution;
+        private System.Windows.Forms.CheckBox cbMotionBlurEnable;
+        private System.Windows.Forms.CheckBox cbWindowMode;
+        private System.Windows.Forms.CheckBox cbShadowEnable;
+        private System.Windows.Forms.Label lbShadowQuality;
+        private System.Windows.Forms.ComboBox cbShadowQuality;
+    }
+}
+
---
+++ trunk/EngineUAB/Code/Configuracion/Form1.cs
@@ -0,0 +1,301 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace Configuracion
+{
+    public partial class Form1 : Form
+    {
+        struct Resol
+        {
+            public int Width;
+            public int Height;
+
+            public Resol(int width, int height)
+            {
+                Width = width;
+                Height = height;
+            }
+
+            public override bool Equals(object obj)
+            {
+                if (obj.GetType() == typeof(Resol))
+                {
+                    Resol oth = (Resol)obj;
+                    
+                    if (this.Width == oth.Width && this.Height == oth.Height)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+
+                return base.Equals(obj);
+            }
+
+            public override int GetHashCode()
+            {
+                return base.GetHashCode();
+            }
+        };
+
+
+        Dictionary<Resol, Resol> resolDic;
+        Dictionary<Resol, int> wrDic;
+        Dictionary<int, Resol> wrRevDic;
+        Dictionary<int, Resol> ShadowDic;
+        Dictionary<Resol, int> ShadowRevDic;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            InitValues();
+
+            InitConfig();
+
+
+        }
+
+        private void InitValues()
+        {
+            wrDic = new Dictionary<Resol, int>();
+            wrDic.Add(new Resol(640, 360), 0);
+            wrDic.Add(new Resol(800, 600), 1);
+            wrDic.Add(new Resol(1024, 768), 2);
+            wrDic.Add(new Resol(1280, 720), 3);
+            wrDic.Add(new Resol(1280, 1024), 4);
+            wrDic.Add(new Resol(1920, 1080), 5);
+
+            wrRevDic = new Dictionary<int, Resol>();
+            wrRevDic.Add(0, new Resol(640, 360)); 
+            wrRevDic.Add(1, new Resol(800, 600));
+            wrRevDic.Add(2, new Resol(1024, 768));
+            wrRevDic.Add(3, new Resol(1280, 720));
+            wrRevDic.Add(4, new Resol(1280, 1024));
+            wrRevDic.Add(5, new Resol(1920, 1080));
+
+            resolDic = new Dictionary<Resol, Resol>();
+            resolDic.Add(new Resol(640, 360), new Resol(640, 360));
+            resolDic.Add(new Resol(800, 600), new Resol(800, 450));
+            resolDic.Add(new Resol(1024, 768), new Resol(1024, 576));
+            resolDic.Add(new Resol(1280, 720), new Resol(1280, 720));
+            resolDic.Add(new Resol(1280, 1024), new Resol(1280, 720));
+            resolDic.Add(new Resol(1920, 1080), new Resol(1920, 1080));
+
+            ShadowDic = new Dictionary<int, Resol>();
+            ShadowDic.Add(0, new Resol(512, 512));
+            ShadowDic.Add(1, new Resol(1024, 1024));
+            ShadowDic.Add(2, new Resol(2048, 2048));
+            ShadowDic.Add(3, new Resol(4096, 4096));
+
+            ShadowRevDic = new Dictionary<Resol, int>();
+            ShadowRevDic.Add(new Resol(512, 512), 0);
+            ShadowRevDic.Add(new Resol(1024, 1024), 1);
+            ShadowRevDic.Add(new Resol(2048, 2048), 2);
+            ShadowRevDic.Add(new Resol(4096, 4096), 3);
+
+        }
+
+        private void InitConfig()
+        {
+            XmlTextReader engineReader = new XmlTextReader("./Data/XML/engine.xml");
+                        
+            while (engineReader.Read())
+            {
+                if (engineReader.NodeType == XmlNodeType.Element && engineReader.Name == "WindowResolution")
+                {
+                    Resol resWR = new Resol();
+
+                    while (engineReader.MoveToNextAttribute())
+                    {
+                        if (engineReader.NodeType == XmlNodeType.Attribute && engineReader.Name == "width")
+                        {
+                            resWR.Width = engineReader.ReadContentAsInt();
+                        }
+                        if (engineReader.NodeType == XmlNodeType.Attribute && engineReader.Name == "height")
+                        {
+                            resWR.Height = engineReader.ReadContentAsInt();
+                        }
+                    }
+
+                    cbResolution.SelectedIndex = wrDic[resWR];
+
+                    break;
+                }
+
+
+                if (engineReader.NodeType == XmlNodeType.Element && engineReader.Name == "RenderMode")
+                {
+                    bool windowed = false;
+
+                    while (engineReader.MoveToNextAttribute())
+                    {
+                        if (engineReader.NodeType == XmlNodeType.Attribute && engineReader.Name == "fullscreenMode")
+                        {
+                            windowed = !engineReader.ReadContentAsBoolean();
+                        }
+                    }
+
+                    cbWindowMode.Checked = windowed;
+                }
+            }
+
+            engineReader.Close();
+
+            engineReader = new XmlTextReader("./Data/XML/lights.xml");
+
+            while (engineReader.Read())
+            {
+                if (engineReader.NodeType == XmlNodeType.Element && engineReader.Name == "directional")
+                {
+                    Resol resSQ = new Resol();
+                    bool genShadow = false;
+
+                    while (engineReader.MoveToNextAttribute())
+                    {
+                        if (engineReader.NodeType == XmlNodeType.Attribute && engineReader.Name == "generate_shadow_map")
+                        {
+                            genShadow = engineReader.ReadContentAsBoolean();
+                        }
+                        if (engineReader.NodeType == XmlNodeType.Attribute && engineReader.Name == "shadow_map_width")
+                        {
+                            resSQ.Width = engineReader.ReadContentAsInt();
+                        }
+                        if (engineReader.NodeType == XmlNodeType.Attribute && engineReader.Name == "shadow_map_height")
+                        {
+                            resSQ.Height = engineReader.ReadContentAsInt();
+                        }
+                    }
+
+                    cbShadowQuality.SelectedIndex = ShadowRevDic[resSQ];
+
+                    cbShadowEnable.Checked = genShadow;
+
+                    break;
+                }
+            }
+
+            engineReader.Close();
+        }
+
+        private void bApply_Click(object sender, EventArgs e)
+        {
+            WriteToEngine();
+
+            WriteToLight();
+        }
+
+        private void WriteToLight()
+        {
+            XmlTextReader engineReader = new XmlTextReader("./Data/XML/lights.xml");
+
+            engineReader.Read();
+
+            XmlDocument xmlDoc = new XmlDocument();
+
+            xmlDoc.Load(engineReader);
+
+            engineReader.Close();
+
+            XmlNode elem = xmlDoc.FirstChild;
+
+            for (; elem != null; elem = elem.NextSibling)
+            {
+                if (elem.NodeType == XmlNodeType.Element && elem.Name == "lights")
+                {
+                    elem = elem.ChildNodes[0];
+                }
+
+                if (elem.NodeType == XmlNodeType.Element && elem.Name == "directional")
+                {
+                    Resol sqRes = ShadowDic[cbShadowQuality.SelectedIndex];
+                    bool shadowEnable = cbShadowEnable.Checked;
+
+                    elem.Attributes["generate_shadow_map"].InnerText = shadowEnable.ToString();
+
+                    elem.Attributes["shadow_map_width"].InnerText = sqRes.Width.ToString();
+                    elem.Attributes["shadow_map_height"].InnerText = sqRes.Height.ToString();
+
+                    break;
+                }
+            }
+
+            XmlTextWriter engineWriter = new XmlTextWriter("./Data/XML/lights.xml", null);
+
+            xmlDoc.WriteTo(engineWriter);
+
+            engineWriter.Flush();
+
+            engineWriter.Close();
+        }
+
+        private void WriteToEngine()
+        {
+            XmlTextReader engineReader = new XmlTextReader("./Data/XML/engine.xml");
+
+            engineReader.Read();
+
+            XmlDocument xmlDoc = new XmlDocument();
+
+            xmlDoc.Load(engineReader);
+
+            engineReader.Close();
+
+            XmlNode elem = xmlDoc.FirstChild;
+            Resol wrRes = wrRevDic[cbResolution.SelectedIndex];
+            Resol wrScreen = resolDic[wrRes];
+
+            for (; elem != null; elem = elem.NextSibling)
+            {
+                if (elem.NodeType == XmlNodeType.Element && elem.Name == "Config")
+                {
+                    elem = elem.ChildNodes[0];
+                }
+
+                if (elem.NodeType == XmlNodeType.Element && elem.Name == "WindowResolution")
+                {
+
+                    elem.Attributes["width"].InnerText = wrRes.Width.ToString();
+                    elem.Attributes["height"].InnerText = wrRes.Height.ToString();
+                }
+
+                if (elem.NodeType == XmlNodeType.Element && elem.Name == "ScreenResolution")
+                {
+                    elem.Attributes["width"].InnerText = wrScreen.Width.ToString();
+                    elem.Attributes["height"].InnerText = wrScreen.Height.ToString();
+                }
+
+                if (elem.NodeType == XmlNodeType.Element && elem.Name == "RenderMode")
+                {
+                    bool windowed = !cbWindowMode.Checked;
+
+                    elem.Attributes["fullscreenMode"].InnerText = windowed.ToString();
+                }
+            }
+
+            XmlTextWriter engineWriter = new XmlTextWriter("./Data/XML/engine.xml", null);
+
+            xmlDoc.WriteTo(engineWriter);
+
+            engineWriter.Flush();
+
+            engineWriter.Close();
+        }
+
+        private void bExit_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
