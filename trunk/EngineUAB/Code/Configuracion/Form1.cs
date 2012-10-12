4d2b293a9bb680d2acf160ee293ec29c191e8059
chaconc@gmail.com <chaconc@gmail.com@38b49c57-6b6f-6c24-9f89-8eb2c450b9d7>
2012-10-12 18:47:42 +0000

    Agregando mensaje ca configuracion

--- trunk/EngineUAB/Code/Configuracion/Form1.cs
+++ trunk/EngineUAB/Code/Configuracion/Form1.cs
@@ -202,6 +202,8 @@ namespace Configuracion
             WriteToEngine();
 
             WriteToLight();
+
+            MessageBox.Show("Changes Applied Successfully");
         }
 
         private void WriteToLight()
