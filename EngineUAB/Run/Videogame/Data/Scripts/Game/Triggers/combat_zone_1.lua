7920b76ea24031647ffa10545fc8b86de4b54860
jordi.arenas.vigo <jordi.arenas.vigo@38b49c57-6b6f-6c24-9f89-8eb2c450b9d7>
2012-10-16 00:18:12 +0000

    Arreglado que se activen y desactiven los muros después de cargarse enemigos de cada zona

--- EngineUAB/Run/Videogame/Data/Scripts/Game/Triggers/combat_zone_1.lua
+++ EngineUAB/Run/Videogame/Data/Scripts/Game/Triggers/combat_zone_1.lua
@@ -13,10 +13,11 @@ class 'CCombatZone1Trigger' (CBoxTrigger)
 		self.enemy_appeared = 1
 		self.total_enemies  = 3 	-- Total de enemigos en la lista
 		self.is_appeared = false
+		self.first_time_entered = false -- Nos indica si es la primera vez que entra para evitar que vuelva a cargar enemigos y pete lua
 	end
 	
 	function CCombatZone1Trigger:update(elapsed_time)
-		--print_logger(0, "CCombatZone1Trigger:update -> Actualizacion trigger zona de combate")
+		print_logger(0, "CCombatZone1Trigger:update -> Actualizacion trigger zona de combate")
 		
 		local t = { "enemy2", "enemy3", "enemy4" }
 		
@@ -44,9 +45,7 @@ class 'CCombatZone1Trigger' (CBoxTrigger)
 				--if ( get_game_process():get_character_manager():get_enemy_by_name(t[self.enemy_appeared]).enable == false ) then
 					get_game_process():get_character_manager():get_enemy_by_name(t[self.enemy_appeared]).enable = true
 					
-					if (self.enemy_appeared == self.total_enemies ) then
-						self.active = false
-					else
+					if (self.enemy_appeared ~= self.total_enemies ) then
 						self.enemy_appeared = self.enemy_appeared + 1
 						self.action_time:init_action()
 						self.action_time:start_action()
@@ -56,23 +55,56 @@ class 'CCombatZone1Trigger' (CBoxTrigger)
 				self.enable_action_time:update(elapsed_time)
 			end 
 		end
+
+		if ( self:are_enemies_dead() == true ) then
+			print_logger(0, "CCombatZone1Trigger:update -> Eliminamos el fuego")
+		
+			core:get_limit_manager():activate_group("zona1", false)
+			self.active = false
+		end
 	end
 	
 	function CCombatZone1Trigger:on_enter(user_data)
-		local process = get_game_process()
-		local l_player_user_data = process:get_character_manager():get_player().physic_controller.user_data
+		if ( self.first_time_entered == false ) then 	
 		
-		if( core:get_physics_manager():compare_user_data(user_data, l_player_user_data) ) then
-			self.action_time:start_action()
-			self.enemy_appeared = 1
-			self.is_appeared = false
-		end 
+			local process = get_game_process()
+			local l_player_user_data = process:get_character_manager():get_player().physic_controller.user_data
+			
+			if( core:get_physics_manager():compare_user_data(user_data, l_player_user_data) ) then
+				self.action_time:start_action()
+				self.enemy_appeared = 1
+				self.is_appeared = false
+			end 
 
-		core:get_limit_manager():activate_group("zona1", true)
+			core:get_limit_manager():activate_group("zona1", true)
+			self.first_time_entered = true
+		end 
 	end
 	
 	function CCombatZone1Trigger:on_stay(user_data)
 	end
 	
 	function CCombatZone1Trigger:on_exit(user_data)
+	end
+	
+	function CCombatZone1Trigger:are_enemies_dead()
+		local l_previous_enemies_to_die = { "enemy2", "enemy3", "enemy4" }
+		local l_total_enemies_to_die = 3
+		
+		local l_are_dead = false
+		
+		for i=1,l_total_enemies_to_die do 
+			local l_enemy = get_game_process():get_character_manager():get_enemy_by_name(l_previous_enemies_to_die[i])
+			if ( l_enemy:is_alive() ) then
+				print_logger(0, "CCombatZone1Trigger:update -> Enemigo vivo")
+		
+				l_are_dead = false
+				break
+			else 
+				print_logger(0, "CCombatZone1Trigger:update -> Enemigo muerto")
+		
+				l_are_dead = true
+			end  	
+		end 
+		return l_are_dead
 	end
--- EngineUAB/Run/Videogame/Data/Scripts/Game/Triggers/combat_zone_6.lua
+++ EngineUAB/Run/Videogame/Data/Scripts/Game/Triggers/combat_zone_6.lua
@@ -12,6 +12,7 @@ class 'CCombatZone6Trigger' (CBoxTrigger)
 		self.enemy_appeared = 1
 		self.total_enemies  = 4 	-- Total de enemigos en la lista
 		self.is_appeared = false
+		self.first_time_entered = false -- Nos indica si es la primera vez que entra para evitar que vuelva a cargar enemigos y pete lua
 	end
 	
 	function CCombatZone6Trigger:update(elapsed_time)
@@ -42,9 +43,7 @@ class 'CCombatZone6Trigger' (CBoxTrigger)
 				--if ( get_game_process():get_character_manager():get_enemy_by_name(t[self.enemy_appeared]).enable == false ) then
 					get_game_process():get_character_manager():get_enemy_by_name(t[self.enemy_appeared]).enable = true
 					
-					if (self.enemy_appeared == self.total_enemies ) then
-						self.active = false
-					else
+					if (self.enemy_appeared ~= self.total_enemies ) then
 						self.enemy_appeared = self.enemy_appeared + 1
 						self.action_time:init_action()
 						self.action_time:start_action()
@@ -54,23 +53,50 @@ class 'CCombatZone6Trigger' (CBoxTrigger)
 				self.enable_action_time:update(elapsed_time)
 			end 
 		end
+		
+		if ( self:are_enemies_dead() ) then 
+			core:get_limit_manager():activate_group("zona6", false)
+			self.active = false
+		end
 	end
 	
 	function CCombatZone6Trigger:on_enter(user_data)
-		local process = get_game_process()
-		local l_player_user_data = process:get_character_manager():get_player().physic_controller.user_data
+		if ( self.first_time_entered == false ) then 	
 		
-		if( core:get_physics_manager():compare_user_data(user_data, l_player_user_data) ) then
-			self.action_time:start_action()
-			self.enemy_appeared = 1
-			self.is_appeared = false
-		end 
+			local process = get_game_process()
+			local l_player_user_data = process:get_character_manager():get_player().physic_controller.user_data
+			
+			if( core:get_physics_manager():compare_user_data(user_data, l_player_user_data) ) then
+				self.action_time:start_action()
+				self.enemy_appeared = 1
+				self.is_appeared = false
+			end 
 
-		core:get_limit_manager():activate_group("zona6", true)
+			core:get_limit_manager():activate_group("zona6", true)
+			self.first_time_entered = true
+		end 
 	end
 	
 	function CCombatZone6Trigger:on_stay(user_data)
 	end
 	
 	function CCombatZone6Trigger:on_exit(user_data)
+	end
+	
+	function CCombatZone6Trigger:are_enemies_dead()
+		local l_previous_enemies_to_die = { "enemy20", "enemy21", "enemy22", "enemy23" }
+		local l_total_enemies_to_die = 4
+		
+		local l_are_dead = false
+		
+		for i=1,l_total_enemies_to_die do 
+			local l_enemy = get_game_process():get_character_manager():get_enemy_by_name(l_previous_enemies_to_die[i])
+			if ( l_enemy:is_alive() ) then
+				l_are_dead = false
+				break
+			else 
+				l_are_dead = true
+			end  	
+		end 
+		return l_are_dead
 	end
