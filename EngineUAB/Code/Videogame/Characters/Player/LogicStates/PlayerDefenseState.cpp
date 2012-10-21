ed538347050fff68b79d16237295dbf4f35468a5
jordi.arenas.vigo <jordi.arenas.vigo@38b49c57-6b6f-6c24-9f89-8eb2c450b9d7>
2012-10-21 12:05:50 +0000

    Añadido retroceso en el defense

--- EngineUAB/Code/Videogame/Characters/Player/LogicStates/PlayerDefenseState.cpp
+++ EngineUAB/Code/Videogame/Characters/Player/LogicStates/PlayerDefenseState.cpp
@@ -7,6 +7,7 @@
 #include "GameProcess.h"
 #include "Characters/Player/Player.h"
 #include "SoundManager.h"
+#include "Utils\Timer.h"
 #include "Logger\Logger.h"
 #include "Core.h"
 #include "Base.h"
@@ -38,6 +39,9 @@ void CPlayerDefenseState::OnEnter( CCharacter* _pCharacter )
 		CORE->GetDebugGUIManager()->GetDebugRender()->SetStateName("Defense");
 	}
 #endif
+
+	m_IsAttacked = false;
+	m_DoubleHit = false;
 }
 
 void CPlayerDefenseState::Execute( CCharacter* _pCharacter, float _fElapsedTime )
@@ -48,7 +52,40 @@ void CPlayerDefenseState::Execute( CCharacter* _pCharacter, float _fElapsedTime 
 		_pCharacter->GetGraphicFSM()->ChangeState( _pCharacter->GetAnimationState("animidle") );
 	}
 
-	_pCharacter->GetController()->Move( v3fZERO, _fElapsedTime);
+	// -----------------------------------------------------
+	// Si no golpean simplemente pongo en modo defensa
+	// -----------------------------------------------------
+	if (!m_IsAttacked )
+	{
+		_pCharacter->GetController()->Move( v3fZERO, _fElapsedTime);
+		return;
+	}
+
+	// ---------------------------------------
+	// En caso de golpeo mientras defiendo
+	// ---------------------------------------
+	
+	// En caso que recibamos otro hit mientras estemos en este estado ampliamos el recorrido
+	if ( m_DoubleHit )
+	{
+		LOGGER->AddNewLog(ELL_INFORMATION,"CPlayerDefenseState::Execute-> Double Hit done!");	
+		CalculateRecoilDirection(_pCharacter);
+		
+		// Esto permite que ya no reste vida ni se recalcule nada
+		m_DoubleHit = false;
+	}
+
+	// Gestiono el retroceso del hit
+	float l_Distance = _pCharacter->GetPosition().Distance(m_InitialHitPoint);
+	if ( l_Distance >= m_MaxHitDistance ) 
+	{
+		_pCharacter->GetSteeringEntity()->SetVelocity(Vect3f(0,0,0));
+		_pCharacter->MoveTo2( _pCharacter->GetSteeringEntity()->GetVelocity(), _fElapsedTime );
+	} 
+	else
+	{
+		_pCharacter->MoveTo2(m_HitDirection, _fElapsedTime );
+	}
 }
 
 void CPlayerDefenseState::OnExit( CCharacter* _pCharacter )
@@ -59,22 +96,64 @@ bool CPlayerDefenseState::OnMessage( CCharacter* _pCharacter, const STelegram& _
 {
 	CORE->GetSoundManager()->PlayEvent( _pCharacter->GetSpeakerName(), "Play_EFX_Caperucita_combo_hit");
 
-
-	// TODO:: HACER SONIDO DE BLOQUEO!
-	if( _Message.Msg == Msg_Attack )
+ 	if ( _Message.Msg == Msg_Attack || _Message.Msg == Msg_Push ) 
 	{
+		m_Message = _Message;
+		CGameProcess *l_pProcess = static_cast<CGameProcess*>(CORE->GetProcess());
+		m_pEnemy				 = l_pProcess->GetCharactersManager()->GetCharacterById(m_Message.Sender);
+
+		CalculateRecoilDirection(_pCharacter);
+		_pCharacter->FaceToForPlayer( m_pEnemy->GetPosition(), CORE->GetTimer()->GetElapsedTime() );
+
+		if (m_IsAttacked)
+		{
+			m_DoubleHit = true;
+			LOGGER->AddNewLog(ELL_INFORMATION,"CPlayerDefenseState::OnMessage-> Posem el doble hit!");	
+		}
+		else
+		{
+			m_IsAttacked = true;
+		}
 		return true;
 	}
-	else if( _Message.Msg == Msg_Push )
-	{
-		/*CCharacter *l_pEnemy	= static_cast<CGameProcess*>(CORE->GetProcess())->GetCharactersManager()->GetCharacterById(_Message.Sender);
-		
-		sDireccion * l_Info = (struct sDireccion *) _Message.ExtraInfo;
-		_pCharacter->MoveTo2(l_Info->Direccion * 1.2f, l_Info->ElapsedTime);
-		_pCharacter->GetSteeringEntity()->SetVelocity(Vect3f(0,0,0));*/
-		LOGGER->AddNewLog(ELL_INFORMATION, "CPlayerDefenseState::OnMessage -> PUSHED!!");
-		return true;
-	}
-
 	return false;
+}
+
+void CPlayerDefenseState::CalculateRecoilDirection( CCharacter * _pCharacter ) 
+{
+	LOGGER->AddNewLog( ELL_WARNING, "CPlayerDefenseState::CalculateRecoilDirection" );
+
+	// Calculamos la dirección y fuerza de retroceso a partir del tipo de mensaje recibido
+	CProperties * l_Properties = _pCharacter->GetProperties();
+	m_MaxHitSpeed = l_Properties->GetHitRecoilSpeed();
+	m_MaxHitDistance = l_Properties->GetHitRecoilDistance();
+	m_InitialHitPoint = _pCharacter->GetPosition();
+	
+	// Cojemos la dirección que se recibe del atacante 
+	Vect3f l_EnemyFront = m_pEnemy->GetFront();
+	Vect3f l_PlayerFront = _pCharacter->GetFront();
+	_pCharacter->SetLocked(true);
+	
+	if ( m_Message.Msg == Msg_Attack )
+	{
+		// Si no tiene velocidad el player rotamos hacia el enemigo y reculamos en su dirección
+		if ( _pCharacter->GetSteeringEntity()->GetSpeed() == 0 )
+		{
+			//m_HitDirection = l_EnemyFront.RotateY(mathUtils::PiTimes(1.f));	
+			m_HitDirection = l_EnemyFront;
+			m_HitDirection *= m_MaxHitSpeed;
+		}
+		// Si tenemos los dos velocidad el vector resultante indicará la dirección a seguir y recular
+		else
+		{
+			// Dirección segun la suma de vectores
+			m_HitDirection = l_EnemyFront + l_PlayerFront;
+		}
+		LOGGER->AddNewLog( ELL_WARNING, "m_Message.Msg == Msg_Attack" );
+	}
+	else if ( ( m_Message.Msg == Msg_Push ) )
+	{
+		m_HitDirection = m_pEnemy->GetSteeringEntity()->GetVelocity();
+		m_PushHit = true; 
+	}
 }
