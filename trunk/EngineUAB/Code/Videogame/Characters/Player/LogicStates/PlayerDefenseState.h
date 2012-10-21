28bdbe357e89fd1c9e38d76fec47ab3edfd1c112
jordi.arenas.vigo <jordi.arenas.vigo@38b49c57-6b6f-6c24-9f89-8eb2c450b9d7>
2012-10-21 12:05:50 +0000

    Añadido retroceso en el defense

--- trunk/EngineUAB/Code/Videogame/Characters/Player/LogicStates/PlayerDefenseState.h
+++ trunk/EngineUAB/Code/Videogame/Characters/Player/LogicStates/PlayerDefenseState.h
@@ -22,8 +22,26 @@ public:
 
 	bool	OnMessage	(  CCharacter* _pCharacter,  const STelegram& _Message );
 
+	void	CalculateRecoilDirection	( CCharacter * _pCharacter );
+
+	void	SetDoubleHit				( bool _DoubleHit )			{ m_DoubleHit = _DoubleHit; }
+	bool	GetDoubleHit				( void ) const				{ return m_DoubleHit; }
+
 private:
 	CActionToInput *m_pInput;
+
+	Vect3f					m_HitDirection;			// Dirección hacia atrás donde se dirige en el golpeo
+	Vect3f					m_HitMaxPosition;		// Posición final del golpeo
+	float					m_MaxHitDistance;		// Distancia máxima de recorrido del hit
+	float					m_MaxHitSpeed;			// Velocidad máxima de recorrido del hit
+	Vect3f					m_InitialHitPoint;		// Punto final de retroceso
+
+	STelegram				m_Message;				// Mensaje del impacto
+	CCharacter *			m_pEnemy;				// Enemigo que me pega
+
+	bool					m_DoubleHit;			// Indica si se ha recibido otro golpe después del primero
+	bool					m_IsAttacked;			// Indica si se ha recibido ya el Push o Attack
+	bool					m_PushHit;				// Indica si se ha recibido el Push 
 };
 
 #endif //_PLAYER_DEFENSE_STATE_H_
